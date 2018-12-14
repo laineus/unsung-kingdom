9e0c3abd0d5929b59f8b62ad0826a91e46ba3629
laineus <lainen000@gmail.com>
2018-12-15 01:32:07 +0900

    walk anim

--- src/js/Player.js
+++ src/js/Player.js
@@ -3,6 +3,10 @@ export default class Player extends Phaser.GameObjects.Sprite {
     super(scene, x, y, 'player')
     scene.physics.world.enable(this)
     scene.add.existing(this)
+    scene.anims.create({ key: 'walkDown', frames: scene.anims.generateFrameNumbers('player', { start: 0, end: 2 }), repeat: -1, frameRate: 5 })
+    scene.anims.create({ key: 'walkLeft', frames: scene.anims.generateFrameNumbers('player', { start: 3, end: 5 }), repeat: -1, frameRate: 5 })
+    scene.anims.create({ key: 'walkRight', frames: scene.anims.generateFrameNumbers('player', { start: 6, end: 8 }), repeat: -1, frameRate: 5 })
+    scene.anims.create({ key: 'walkUp', frames: scene.anims.generateFrameNumbers('player', { start: 9, end: 11 }), repeat: -1, frameRate: 5 })
   }
   update () {
     this._walkToTargetPosition()
@@ -19,9 +23,17 @@ export default class Player extends Phaser.GameObjects.Sprite {
   }
   _walkToTargetPosition () {
     this.body.setVelocity(0)
-    if (Math.hypot(this.diffToTargetPositionX, this.diffToTargetPositionY) < 1) return
+    if (Math.hypot(this.diffToTargetPositionX, this.diffToTargetPositionY) < 1) {
+      if (this.anims.currentAnim) this.setFrame(this.anims.currentAnim.frames[1].textureFrame)
+      return
+    }
     this.body.setVelocityX(this.diffToTargetPositionX)
     this.body.setVelocityY(this.diffToTargetPositionY)
     this.body.velocity.normalize().scale(100)
+    if (Math.abs(this.body.velocity.x) > Math.abs(this.body.velocity.y)) {
+      this.anims.play(this.body.velocity.x > 0 ? 'walkRight' : 'walkLeft', true)
+    } else {
+      this.anims.play(this.body.velocity.y > 0 ? 'walkDown' : 'walkUp', true)
+    }
   }
 }
