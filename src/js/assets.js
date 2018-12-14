8950a281deb16c233f8c9e84265f008091bc76ce
laineus <lainen000@gmail.com>
2018-12-15 01:22:52 +0900

    fix asset load

--- src/js/assets.js
+++ src/js/assets.js
@@ -1,12 +1,14 @@
 export default {
-  image: {
-    town: '../img/town.png',
-    player: '../img/player.png'
-  },
-  json: {
-    town: '../map/town.json'
-  },
-  tilemapTiledJSON: {
-    first: '../map/first.json'
-  }
+  image: [
+    ['town', '../img/town.png']
+  ],
+  json: [
+    ['town', '../map/town.json']
+  ],
+  tilemapTiledJSON: [
+    ['first', '../map/first.json']
+  ],
+  spritesheet: [
+    ['player', '../img/player.png', { frameWidth: 32, frameHeight: 32, endFrame: 12 }]
+  ]
 }
--- src/js/loadAssets.js
+++ src/js/loadAssets.js
@@ -1,6 +1,2 @@
 import assets from './assets'
-export default scene => {
-  Object.keys(assets).forEach(method => {
-    Object.keys(assets[method]).forEach(key => scene.load[method](key, assets[method][key]))
-  })
-}
+export default scene => Object.keys(assets).forEach(method => assets[method].forEach(args => scene.load[method](...args)))
