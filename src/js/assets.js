e7542fc92ee3967e9a35dba9895367a47a3289bb
laineus <lainen000@gmail.com>
2018-12-29 19:59:43 +0900

    add town1

--- src/js/assets.js
+++ src/js/assets.js
@@ -1,15 +1,18 @@
 export default {
   image: [
     ['room', '../img/room.png'],
+    ['town', '../img/town.png'],
     ['ann', '../img/ann.png'],
     ['francisca', '../img/francisca.png'],
     ['jaquelyn', '../img/jaquelyn.png']
   ],
   json: [
-    ['room', '../map/room.json']
+    ['room', '../map/room.json'],
+    ['town', '../map/town.json']
   ],
   tilemapTiledJSON: [
-    ['room1', '../map/room1.json']
+    ['room1', '../map/room1.json'],
+    ['town1', '../map/town1.json']
   ],
   spritesheet: [
     ['player', '../img/player.png', { frameWidth: 32, frameHeight: 32, endFrame: 12 }]
