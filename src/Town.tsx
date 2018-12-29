d304df0ab62ae0058768084a9e3ad39920318ceb
laineus <lainen000@gmail.com>
2018-12-29 22:14:40 +0900

    delete unnecessary file

--- src/Town.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Town" tilewidth="16" tileheight="16" tilecount="528" columns="8">
- <image source="mapchip.png" width="128" height="1056"/>
-</tileset>
