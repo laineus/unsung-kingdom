e69c8e645c152c7e5edf61c9ce6f4819d9fc838d
laineus <lainen000@gmail.com>
2018-12-31 01:51:35 +0900

    room chara

--- src/map/room1.js
+++ src/map/room1.js
@@ -4,7 +4,7 @@ export default {
   create () {
     new Gate(this, [18, 23, 19, 24], 'town1', [17, 35])
     // npc
-    this.npc = new Character(this, 160, 210, 'player')
+    this.npc = new Character(this, (12).toPixel, (17).toPixel, 'player')
     this.npc.setTalk()
     this.map.addCollider(this.npc)
     this.physics.add.collider(this.player, this.npc)
