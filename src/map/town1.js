9dfbc0ac2b771ee51f802df213143c3187b7ea25
laineus <lainen000@gmail.com>
2018-12-31 01:27:30 +0900

    back to room

--- src/map/town1.js
+++ src/map/town1.js
@@ -3,7 +3,11 @@ export default {
   create () {
     // doorToRoom
     this.doorToRoom = new Substance(this, (17).toPixel, (34).toPixel)
-    this.doorToRoom.setTalk()
+    this.doorToRoom.setEvent(() => {
+      this.scene.get('UI').transition(() => {
+        this.scene.start('Game', { map: 'room1', x: (18).toPixel, y: (20).toPixel })
+      })
+    })
   },
   update () {
     this.doorToRoom.update()
