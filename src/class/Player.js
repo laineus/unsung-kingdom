fcbc1a221fbbc984555b4ef1298b1e75bb0d22d6
laineus <lainen000@gmail.com>
2018-12-30 22:31:58 +0900

    targetMarker

--- src/class/Player.js
+++ src/class/Player.js
@@ -2,8 +2,12 @@ import Character from './Character'
 export default class Player extends Character {
   constructor (scene, x, y) {
     super(scene, x, y, 'player')
+    this.targetMarker = scene.add.circle(0, 0, 5, 0xAA0000, 0.5)
+    this.add(this.targetMarker)
   }
   update () {
     super.update()
+    this.targetMarker.visible = this.hasTargetPosition
+    this.targetMarker.setPosition(this.diffToTargetPositionX, this.diffToTargetPositionY)
   }
 }
