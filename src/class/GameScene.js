8ed9f12a4fcfd09789b02108a92c2b660cca25f1
laineus <lainen000@gmail.com>
2018-12-31 00:13:33 +0900

    debugAction

--- src/class/GameScene.js
+++ src/class/GameScene.js
@@ -1,6 +1,7 @@
 import Player from './Player'
 import GameMap from './GameMap'
 import maps from '../data/maps'
+import pixelToTile from '../util/pixelToTile'
 export default class GameScene extends Phaser.Scene {
   constructor () {
     super({ key: 'Game', active: false })
@@ -25,9 +26,16 @@ export default class GameScene extends Phaser.Scene {
     this.input.on('pointermove', walk)
     this.event = maps[payload.map]
     if (this.event) this.event.create.call(this)
+    // debug
+    this.setDebugAction()
   }
   update (time, delta) {
     this.player.update()
     if (this.event) this.event.update.call(this)
+  }
+  setDebugAction () {
+    this.input.keyboard.on('keydown_I', () => {
+      console.log(`x: ${this.player.x}, y: ${this.player.y}, tileX: ${pixelToTile(this.player.x)}, tileY: ${pixelToTile(this.player.y)}`)
+    })
   }
 }
