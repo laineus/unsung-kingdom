fbec3a55a5b8389cb5fc75fae88fb1e6c22f510a
laineus <lainen000@gmail.com>
2019-01-02 00:35:12 +0900

    add Enemy class

---
+++ src/class/Enemy.js
@@ -0,0 +1,9 @@
+import Character from './Character'
+export default class Enemy extends Character {
+  constructor (scene, x, y) {
+    super(scene, x, y, 'player')
+  }
+  update () {
+    super.update()
+  }
+}
