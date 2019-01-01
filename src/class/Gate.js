ed6e63e7bd4f0dc872c4caec4ac45067fa4d39a4
laineus <lainen000@gmail.com>
2019-01-01 17:21:56 +0900

    bugfix

--- src/class/Gate.js
+++ src/class/Gate.js
@@ -1,10 +1,8 @@
 export default class Gate extends Phaser.GameObjects.Zone {
   constructor (scene, key, x, y, area) {
-    const x = area[0].toPixel
-    const y = area[1].toPixel
     const width = (area[2] + 1 - area[0]).toPixel
     const height = (area[3] + 1 - area[1]).toPixel
-    super(scene, x, y, width, height)
+    super(scene, area[0].toPixel, area[1].toPixel, width, height)
     this.setOrigin(0, 0)
     scene.physics.world.enable(this)
     scene.physics.add.overlap(this, scene.player, () => {
