87d012f35918c0ae71ab3b11158d879d04ba9244
laineus <lainen000@gmail.com>
2019-01-02 22:01:40 +0900

    Gauge

---
+++ src/class/Gauge.js
@@ -0,0 +1,22 @@
+export default class Gauge extends Phaser.GameObjects.Container {
+  constructor (scene, width, height, valueMax = 100) {
+    super(scene)
+    this.scene = scene
+    scene.add.existing(this)
+    this.bg = scene.add.rectangle(0, 0, width, height, 0x222222).setOrigin(0, 0.5).setPosition(width / -2, 0)
+    this.bar = scene.add.rectangle(0, 0, width, height, 0xCC1111).setOrigin(0, 0.5).setPosition(width / -2, 0)
+    this.add([this.bg, this.bar])
+    this.valueMax = valueMax
+    this.value = valueMax
+  }
+  get value () {
+    return this._value
+  }
+  set value (value) {
+    this._value = value
+    this.bar.setScale(this.barScale, 1)
+  }
+  get barScale () {
+    return this.value / this.valueMax
+  }
+}
