ae1cae1c108a6386cf0c773f36f055ccfd7e721c
laineus <lainen000@gmail.com>
2019-01-05 00:08:23 +0900

    assets

--- src/data/assets.js
+++ src/data/assets.js
@@ -16,7 +16,14 @@ export default {
     ['room1', '../map/room1.json'],
     ['town1', '../map/town1.json'],
     ['forest1', '../map/forest1.json'],
-    ['forest2', '../map/forest2.json']
+    ['forest2', '../map/forest2.json'],
+    ['forest3', '../map/forest3.json'],
+    ['forest4', '../map/forest4.json'],
+    ['forest5', '../map/forest5.json'],
+    ['forest6', '../map/forest6.json'],
+    ['forest7', '../map/forest7.json'],
+    ['forest8', '../map/forest8.json'],
+    ['forest9', '../map/forest9.json']
   ],
   spritesheet: [
     ['player', '../img/player.png', { frameWidth: 32, frameHeight: 32, endFrame: 12 }]
