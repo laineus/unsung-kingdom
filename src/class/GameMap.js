a8d0482880fc6bdf7af267feed16f92e4d535c9c
laineus <lainen000@gmail.com>
2019-01-10 23:22:03 +0900

    assign

--- src/class/GameMap.js
+++ src/class/GameMap.js
@@ -11,8 +11,8 @@ export default class GameMap {
     }).filter(v => v)
     this.staticLayers.forEach(layer => layer.setCollision(collides))
     scene.physics.add.collider(this.staticLayers, scene.substances)
-    this._getGateObjects().map(gate => new Gate(scene, gate.key, gate.x, gate.y, gate.zone_x, gate.zone_y, gate.zone_width, gate.zone_height))
-    this._getObjects('enemy').map(data => new Enemy(scene, data.x, data.y, data.name))
+    this.gates = this._getGateObjects().map(gate => new Gate(scene, gate.key, gate.x, gate.y, gate.zone_x, gate.zone_y, gate.zone_width, gate.zone_height))
+    this.enemies = this._getObjects('enemy').map(data => new Enemy(scene, data.x, data.y, data.name))
     return this
   }
   get width () {
