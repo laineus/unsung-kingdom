bdb764955ea5c4b9c8cb7a4d8d7f410c8382cc18
laineus <lainen000@gmail.com>
2019-01-17 00:47:57 +0900

    five dogs event

--- src/data/defaultState.js
+++ src/data/defaultState.js
@@ -2,6 +2,10 @@ export default () => {
   return {
     map: 'room1',
     x: 500,
-    y: 600
+    y: 600,
+    field: {},
+    quest: {
+      five: { begin: false, 1: false, 2: false, 3: false, 4: false, 5: false }
+    }
   }
 }
--- src/map/forest2.js
+++ src/map/forest2.js
@@ -1,2 +1,9 @@
 export default {
+  create (scene) {
+    const five = scene.map.getCharaById(4)
+    five.setTalk([
+      ['ann'],
+      { name: 'ann', text: '犬が迷子になってしまったんだ。' }
+    ])
+  }
 }
