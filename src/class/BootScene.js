701e561349d341658c30e22bf56b0f885ddb8e50
laineus <lainen000@gmail.com>
2019-01-16 23:32:40 +0900

    boot saved data

--- src/class/BootScene.js
+++ src/class/BootScene.js
@@ -1,13 +1,15 @@
 import loadAssets from '../util/loadAssets'
 import registerAnims from '../util/registerAnims'
+import storage from '../data/storage'
 export default class GameScene extends Phaser.Scene {
   constructor () {
     super({ key: 'Boot', active: true })
   }
   create () {
     registerAnims(this)
+    const state = storage.state
     this.scene.start('UI')
-    this.scene.start('Game', { map: 'room1', x: 500, y: 600 })
+    this.scene.start('Game', { map: state.map, x: state.x, y: state.y })
   }
   preload () {
     loadAssets(this)
