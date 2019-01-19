a59e6d2daedfc04ff3905213f0b544664efa3735
laineus <lainen000@gmail.com>
2019-01-19 23:38:55 +0900

    select pause

--- src/class/Select.js
+++ src/class/Select.js
@@ -4,6 +4,7 @@ export default class Select extends Phaser.GameObjects.Container {
     this.scene = scene
     this.callback = callback
     scene.add.existing(this)
+    scene.scene.pause('Game')
     this.options = options.map((text, i) => {
       const opt = this.option(text, i).setPosition(0, i * 50)
       this.add(opt)
@@ -15,6 +16,8 @@ export default class Select extends Phaser.GameObjects.Container {
     const label = this.scene.add.text(18, 13, text, { fontSize: 18, lineSpacing: 13 }).setPadding(0, 2, 0, 0)
     option.add([window, label])
     window.on('pointerdown', pointer => {
+      this.scene.scene.resume('Game')
+      this.destroy()
       if (this.callback) this.callback(i)
     })
     return option
