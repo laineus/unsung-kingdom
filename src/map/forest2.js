7de0238f088e4159ccaed70bef35364fc734a949
laineus <lainen000@gmail.com>
2019-01-22 23:24:10 +0900

    sample quest

--- src/map/forest2.js
+++ src/map/forest2.js
@@ -2,18 +2,26 @@ export default {
   create (scene) {
     const five = scene.map.getCharaById(4)
     five.setTapEvent(() => {
-      scene.talk([
-        ['ann'],
-        { name: 'ann', text: '犬が迷子になってしまったんだ。' }
-      ], t => {
-        scene.select(['はい', 'いいえ'], i => {
-          t.destroy()
-          scene.talk([
-            ['ann'],
-            { name: 'ann', text: i === 0 ? 'わーい' : 'しょんぼり' }
-          ])
-        })
-      }, false)
+      if (!scene.storage.state.quest.five.begin) {
+        scene.talk([
+          ['ann'],
+          { name: 'ann', text: '犬が迷子になってしまったんだ。' }
+        ], t => {
+          scene.select(['はい', 'いいえ'], i => {
+            t.destroy()
+            scene.talk([
+              ['ann'],
+              { name: 'ann', text: i === 0 ? 'わーい' : 'しょんぼり' }
+            ])
+            if (i === 0) scene.storage.state.quest.five.begin = true
+          })
+        }, false)
+      } else {
+        scene.talk([
+          ['ann'],
+          { name: 'ann', text: 'よろしく頼んだよー。' }
+        ])
+      }
     })
   }
 }
