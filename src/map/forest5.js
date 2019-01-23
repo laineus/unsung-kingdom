6e25682142bab095c18f7d3934070da1615d25bb
laineus <lainen000@gmail.com>
2019-01-24 00:04:41 +0900

    dog event

--- src/map/forest5.js
+++ src/map/forest5.js
@@ -1,2 +1,18 @@
 export default {
+  create (scene) {
+    const dog = scene.map.getCharaById(3)
+    if (scene.storage.state.quest.five['2'] === 1) {
+      dog.destroy()
+    } else {
+      dog.setTapEvent(() => {
+        if (!scene.storage.state.quest.five.begin) {
+          scene.talk([['ann'], { name: 'ann', text: 'ワン！' }])
+        } else {
+          scene.talk([['ann'], { name: 'ann', text: 'ワンワン！' }])
+          scene.storage.state.quest.five['2'] = 1
+          dog.destroy()
+        }
+      })
+    }
+  }
 }
