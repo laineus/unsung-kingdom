85ba1495bd8114c7d25fb68f184086243ce41852
laineus <lainen000@gmail.com>
2019-06-22 18:31:53 +0900

    battle

--- src/class/Battle.js
+++ src/class/Battle.js
@@ -20,12 +20,14 @@ export default class Battle extends Phaser.GameObjects.Container {
     this.players = this.scene.add.container(0, 0)
     this.add([this.window, this.enemies, this.players])
     // test image
-    this.enemies.add(new EnemyBattler(this.scene, this.tapEnemy.bind(this)).setPosition(config.WIDTH.half, config.HEIGHT.half - 100))
-    this.enemies.add(new EnemyBattler(this.scene, this.tapEnemy.bind(this)).setPosition(config.WIDTH.half, config.HEIGHT.half - 100))
-    this.enemies.add(new EnemyBattler(this.scene, this.tapEnemy.bind(this)).setPosition(config.WIDTH.half, config.HEIGHT.half - 100))
+    const sampleStatus1 = { hp: 35, atk: 4, def: 3, dex: 2, agi: 2 }
+    this.enemies.add(new EnemyBattler(this.scene, sampleStatus1, this.tapEnemy.bind(this)).setPosition(config.WIDTH.half, config.HEIGHT.half - 100))
+    this.enemies.add(new EnemyBattler(this.scene, sampleStatus1, this.tapEnemy.bind(this)).setPosition(config.WIDTH.half, config.HEIGHT.half - 100))
+    this.enemies.add(new EnemyBattler(this.scene, sampleStatus1, this.tapEnemy.bind(this)).setPosition(config.WIDTH.half, config.HEIGHT.half - 100))
     // test image
-    this.players.add(new PlayerBattler(this.scene).setPosition(config.WIDTH.half - 120, config.HEIGHT.half + 100))
-    this.players.add(new PlayerBattler(this.scene).setPosition(config.WIDTH.half + 120, config.HEIGHT.half + 100))
+    const sampleStatus2 = { hp: 50, atk: 15, def: 5, dex: 4, agi: 3 }
+    this.players.add(new PlayerBattler(this.scene, sampleStatus2).setPosition(config.WIDTH.half - 120, config.HEIGHT.half + 100))
+    this.players.add(new PlayerBattler(this.scene, sampleStatus2).setPosition(config.WIDTH.half + 120, config.HEIGHT.half + 100))
     // register
     this.all = []
     this.players.list.forEach(v => this.all.push(v))
@@ -47,19 +49,21 @@ export default class Battle extends Phaser.GameObjects.Container {
   }
   increaseTurn () {
     this.turnIndex = this.turnIndex < (this.all.length - 1) ? this.turnIndex + 1 : 0
-    const result = this.currentBattler.increaseTurn()
+    const result = this.currentBattler.alive && this.currentBattler.increaseTurn()
     if (!result) return this.increaseTurn()
     if (!this.playerTurn) return this.execEnemyTurn()
   }
   execEnemyTurn () {
     setTimeout(() => {
-      this.players.list[0].addDamage(Math.randomInt(10, 20))
+      this.currentBattler.attackTo(this.players.list.random())
+      // this.players.list[0].addDamage(Math.randomInt(10, 20))
       this.increaseTurn()
     }, 1000)
   }
   tapEnemy (enemy) {
     if (!this.playerTurn) return
-    enemy.addDamage(Math.randomInt(60, 200))
+    this.currentBattler.attackTo(enemy)
+    // enemy.addDamage(Math.randomInt(60, 200))
     this.increaseTurn()
   }
   get victory () {
