993c492e44e6cf678eb89312b8cc81d2a937c2eb
laineus <lainen000@gmail.com>
2019-06-23 15:00:48 +0900

    blur

--- src/util/registerShaders.js
+++ src/util/registerShaders.js
@@ -1,5 +1,13 @@
 import CustomPipeline from '../class/CustomPipeline'
+import config from '../data/config'
 import light from '../shader/light'
+import blur from '../shader/blur'
 export default scene => {
+  // light
   scene.game.renderer.addPipeline('light', new CustomPipeline(scene.game, light))
+  // blur
+  const blurPipeline = scene.game.renderer.addPipeline('blur', new CustomPipeline(scene.game, blur))
+  blurPipeline.setFloat1('resolution', config.WIDTH)
+  blurPipeline.setFloat1('radius', 1.0)
+  blurPipeline.setFloat2('dir', 3.0, 3.0)
 }
