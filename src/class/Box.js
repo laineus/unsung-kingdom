9645cac55b358eaae9faf456796128e9ab5b50c2
laineus <lainen000@gmail.com>
2019-06-23 15:43:08 +0900

    box line

--- src/class/Box.js
+++ src/class/Box.js
@@ -1,5 +1,5 @@
 export default class Box extends Phaser.GameObjects.Polygon {
-  constructor (scene, x, y, width, height, color) {
+  constructor (scene, x, y, width, height, color, lineColor = null) {
     // Parallelogram
     const diff = height / 5
     const leftTop = [diff, 0]
@@ -7,5 +7,10 @@ export default class Box extends Phaser.GameObjects.Polygon {
     const rightTop = [width, 0]
     const rightBottom = [width - diff, height]
     super(scene, x, y, [leftTop, rightTop, rightBottom, leftBottom], color)
+    if (lineColor) {
+      this.isStroked = true
+      this.lineWidth = 1
+      this.strokeColor = lineColor
+    }
   }
 }
