5f78cf9e03bb6ef47d1e3ee6bfa69d1925be3f68
laineus <lainen000@gmail.com>
2019-06-25 22:10:01 +0900

    data

---
+++ src/data/battlers.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    name: 'Ann', lv: 1,
+    hp: 100, atk: 4, def: 3, dex: 3, agi: 2
+  },
+  {
+    name: 'Francisca', lv: 1,
+    hp: 100, atk: 3, def: 2, dex: 3, agi: 4
+  },
+  {
+    name: 'Jaquelyn', lv: 1,
+    hp: 100, atk: 2, def: 2, dex: 4, agi: 4
+  },
+  {
+    name: 'トレント', lv: 1,
+    hp: 100, atk: 1, def: 1, dex: 1, agi: 1
+  }
+]
---
+++ src/data/weapons.js
@@ -0,0 +1,3 @@
+export default [
+  { id: 1, name: 'ショートソード', atk: 5, abilities: [] }
+]
