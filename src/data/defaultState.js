b3a4533b562096a5e475005139cb69ec46ee83f0
laineus <lainen000@gmail.com>
2019-06-25 23:28:09 +0900

    fix state

--- src/data/defaultState.js
+++ src/data/defaultState.js
@@ -8,7 +8,7 @@ export default () => {
     quest: {
       five: { started: false, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, completed: false }
     },
-    battlers: battlers.map(v => Object.assign(v, { exp: 0, maxHp: v.hp })),
+    battlers: battlers.filter((_, i) => i < 3).map(v => Object.assign(v, { exp: 0, maxHp: v.hp })),
     items: [{ item_id: 1, count: 5 }],
     weapons: [{ id: 1, weapon_id: 1 }, { id: 2, weapon_id: 1 }]
   }
