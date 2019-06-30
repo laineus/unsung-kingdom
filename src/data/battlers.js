0384051185689a9011a4f5a077bae2571bbaca3b
laineus <lainen000@gmail.com>
2019-06-30 17:51:56 +0900

    up

--- src/data/battlers.js
+++ src/data/battlers.js
@@ -1,15 +1,18 @@
 export default [
   {
     key: 'ann', name: 'Ann', lv: 1,
-    hp: 100, atk: 4, def: 3, dex: 3, agi: 2
+    hp: 100, atk: 4, def: 3, dex: 3, agi: 2,
+    up: { hp: 100, atk: 100, def: 90, dex: 70, agi: 70 }
   },
   {
     key: 'francisca', name: 'Francisca', lv: 1,
-    hp: 100, atk: 3, def: 2, dex: 3, agi: 4
+    hp: 100, atk: 3, def: 2, dex: 3, agi: 4,
+    up: { hp: 85, atk: 85, def: 75, dex: 100, agi: 85 }
   },
   {
     key: 'jaquelyn', name: 'Jaquelyn', lv: 1,
-    hp: 100, atk: 2, def: 2, dex: 4, agi: 4
+    hp: 100, atk: 2, def: 2, dex: 4, agi: 4,
+    up: { hp: 90, atk: 75, def: 80, dex: 85, agi: 100 }
   },
   {
     key: 'torrent', name: 'トレント', lv: 1,
