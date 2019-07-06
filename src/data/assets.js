dd7e23636130241b1c1f0cd4264f3667fa678892
laineus <lainen000@gmail.com>
2019-07-06 22:41:38 +0900

    fix dog

--- src/data/assets.js
+++ src/data/assets.js
@@ -30,7 +30,7 @@ export default {
   ],
   spritesheet: [
     ['player', '../img/player.png', { frameWidth: 25, frameHeight: 41, endFrame: 12 }],
-    ['dog', '../img/dog.png', { frameWidth: 25, frameHeight: 25, endFrame: 12 }],
+    ['dog', '../img/dog.png', { frameWidth: 32, frameHeight: 32, endFrame: 12 }],
     ['boar', '../img/boar.png', { frameWidth: 32, frameHeight: 32, endFrame: 6 }]
   ]
 }
