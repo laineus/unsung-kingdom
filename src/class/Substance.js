c00ca40a5114a9607470d2a2285e6ef92fee5a87
laineus <lainen000@gmail.com>
2019-07-06 21:24:06 +0900

    shadow

--- src/class/Substance.js
+++ src/class/Substance.js
@@ -7,8 +7,10 @@ export default class Substance extends Phaser.GameObjects.Container {
     this.scene = scene
     this.key = key
     this.image = key ? scene.add.sprite(0, 0, key) : scene.add.rectangle(0, 0, config.TILE_SIZE, config.TILE_SIZE)
+    this.image.setPosition(0, -this.image.height.half)
     this.setSize(width || this.image.width, height || this.image.height)
-    this.add(this.image)
+    this.shadow = scene.add.circle(4, 0, 8, 0x000000, 0.3).setScale(1, 0.5)
+    this.add([this.shadow, this.image])
     scene.add.existing(this)
     scene.substances.add(this)
     scene.physics.world.enable(this)
@@ -26,10 +28,10 @@ export default class Substance extends Phaser.GameObjects.Container {
   }
   setTapEvent (balloon = true) {
     const distance = 150
-    this.tapArea = this.scene.add.rectangle(0, -15, this.width + 20, this.height + 50).setInteractive()
+    this.tapArea = this.scene.add.rectangle(0, -this.image.height, this.image.width + 20, this.image.height + 50).setInteractive()
     this.add(this.tapArea)
     if (balloon) {
-      this.balloon = new Baloon(this.scene).setPosition(0, -50)
+      this.balloon = new Baloon(this.scene).setPosition(0, -this.image.height.half - 50)
       this.add(this.balloon)
     }
     this.tapArea.on('pointerdown', pointer => {
