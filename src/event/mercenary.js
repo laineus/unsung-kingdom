4eaca625dbf146c66f001cc2b6b7773b5d56f462
laineus <lainen000@gmail.com>
2019-07-09 22:41:46 +0900

    event file

---
+++ src/event/mercenary.js
@@ -0,0 +1,50 @@
+export const mercenary1 = (scene, leader) => {
+  leader.setDisplayName('負傷した傭兵団長').setTapEvent().on('tap', async chara => {
+    if (scene.storage.state.event.mercenary.started) {
+      scene.talk([{ chara, text: 'きっとそんなに遠くには行っていないはずだ。頼んだぜ。' }])
+    } else {
+      const messages = [
+        [
+          { chara: 'ann', text: '大丈夫ですか？' },
+          { chara, text: 'ああ、大丈夫だ。' },
+          { chara: 'jaquelyn', text: '怪我をしているように見えますけど。' },
+          { chara, text: '人食い熊のレックスベアの討伐に来たんだが、圧倒的に力不足だったようだ。' },
+          { chara, text: '情けねえ。' },
+          { chara: 'ann', text: 'どうしてレックスベアを？' },
+          { chara, text: 'ただの報酬目当てさ。' },
+          { chara, text: '国王の病気が関係してるみたいでな、奴の討伐報酬はかなりのものなんだ。' },
+          { chara: 'jaquelyn', text: '国王の病気がどう関係してるんですか？' },
+          { chara, text: 'この森の奥に住むドリスタンという老人が、王の治療のための調薬を頼まれているんだが、' },
+          { chara, text: 'その材料にレックスベアの死体を要求しているらしい。' },
+          { chara, text: 'なんでかは知らん。' },
+          { chara: 'ann', text: 'そうなんですね。' },
+          { chara, text: 'ところであんたら、' }
+        ],
+        [
+          { chara, text: 'この先で俺達の仲間を見かけたら、この場所を伝えてやってくれないか？' },
+          { chara, text: 'ここまで逃げてくる途中ではぐれちまったんだよ。' },
+          { chara, text: 'おかげでこうして森から撤退できずにいるんだ。' }
+        ]
+      ]
+      const t = await scene.talk(!scene.storage.state.event.mercenary.talked ? messages[0].concat(messages[1], null) : messages[1].concat(null))
+      const i = await scene.select(['はい', 'いいえ'])
+      scene.storage.state.event.mercenary.talked = true
+      t.destroy()
+      scene.talk([{ chara, text: i === 0 ? '助かる。礼ははずませてもらうぞ。' : 'そうか。' }])
+      if (i === 0) scene.storage.state.event.mercenary.started = true
+    }
+  })
+}
+
+export const mercenary2 = (scene, flower) => {
+  if (scene.storage.state.event.mercenary.solved) return flower.destroy()
+  flower.setDisplayName('？').setTapEvent().on('tap', async chara => {
+    if (!scene.storage.state.event.mercenary.started) {
+      scene.talk([{ chara: 'francisca', text: '何この花。へんなの。' }])
+    } else {
+      await scene.ui.battle(['torrent'])
+      scene.talk([{ chara: 'francisca', text: '倒した' }])
+      scene.storage.state.event.mercenary.solved = true
+    }
+  })
+}
