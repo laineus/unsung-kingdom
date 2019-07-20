aeefacec5c26e67f102a237fe330e136d4adcc92
laineus <lainen000@gmail.com>
2019-07-21 00:14:01 +0900

    design

--- src/class/Menu.js
+++ src/class/Menu.js
@@ -4,14 +4,18 @@ export default class Menu extends Phaser.GameObjects.Container {
     super(scene)
     this.scene = scene
     scene.add.existing(this)
-    this.window = scene.add.rectangle(20, 20, config.WIDTH - 40, config.HEIGHT - 40, 0x000000, 0.7).setOrigin(0, 0).setInteractive()
-    this.window.on('pointerdown', () => {
+    this.bg = scene.add.rectangle(0, 0, config.WIDTH, config.HEIGHT, 0x886644, 0.2).setOrigin(0, 0).setInteractive()
+    this.bg.blendMode = 1
+    this.bg.on('pointerdown', () => {
       this.destroy()
     })
-    this.add(this.window)
+    this.window = scene.add.rectangle(20, 20, config.WIDTH - 40, config.HEIGHT - 40, 0x000000, 0.7).setOrigin(0, 0).setInteractive()
+    this.add([this.bg, this.window])
+    this.scene.gameScene.blur(true)
     scene.scene.pause('Game')
   }
   destroy () {
+    this.scene.gameScene.blur(false)
     this.scene.scene.resume('Game')
     super.destroy()
   }
