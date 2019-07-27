8ba143c7612935d5191f0796d43f8478905de2e8
laineus <lainen000@gmail.com>
2019-07-27 17:37:17 +0900

    chapters and missions

---
+++ src/data/chapters.js
@@ -0,0 +1,8 @@
+export default {
+  0: { name: '序章 王殺しのジャック', en: 'Jack the Kingkiller' },
+  1: { name: '1章 ワルコフォレンスの森', en: 'Warcoforence' },
+  2: { name: '2章 トロイア公爵邸の地下通路', en: 'Troy\'s secret passage' },
+  3: { name: '3章 聖アンテルスの墓地', en: 'St Anterus\' catacomb' },
+  4: { name: '4章 グリファルデ神殿', en: 'Temple Grefalde' },
+  5: { name: '終章 平和王エドガー', en: 'Edgar the Peaceful' },
+}
---
+++ src/data/missions.js
@@ -0,0 +1,22 @@
+export default [
+  { chapter: 0, name: '王殺しのジャック', en: 'Jack the Kingkiller' },
+  { chapter: 1, name: 'マシューと狩猟犬', en: 'Matthew and Hunting dogs' },
+  { chapter: 1, name: 'アイザムバード傭兵団の討伐遠征', en: 'Isambard mercenaries' },
+  { chapter: 1, name: '森の賢人ドリスタン', en: 'Philosopher Drystan' },
+  { chapter: 1, name: 'ワルコフォレンスの人食い熊', en: 'The Rex Bear' },
+  { chapter: 2, name: 'メアリー王女と公爵令嬢ロレッタ', en: 'Princess Mary and Loretta' },
+  { chapter: 2, name: '泣くには名誉が足りぬ', en: 'Honor for tears' },
+  { chapter: 2, name: '幽閉されしカサンドラ', en: 'Incarcerated Cassandra' },
+  { chapter: 2, name: 'コルネリウス騎士団長の企て', en: 'The Dirty Knight' },
+  { chapter: 3, name: 'レンフィールド家の大罪', en: 'Sins of the Renfield family' },
+  { chapter: 3, name: 'ダリオの哀しみ', en: 'The Sorrow of Dario' },
+  { chapter: 3, name: 'マリアンヌの歌を聞いてから死ね', en: 'Listen Marianne\'s song before die' },
+  { chapter: 3, name: '呪われしフェルディナンド卿', en: 'Cursed Lord Ferdinand' },
+  { chapter: 3, name: 'エヴァンジェリナ妃の亡霊', en: 'The Ghost of Princess Evangelina' },
+  { chapter: 4, name: '不浄のロレイン像', en: 'Sinning Lorraine\'s statue' },
+  { chapter: 4, name: '女神さまに捧ぐリュート', en: 'Lute for goddess' },
+  { chapter: 4, name: 'すべて世は事もなし', en: 'All\'s right with the world' },
+  { chapter: 4, name: '聖剣カリブルヌス', en: 'Caliburnus' },
+  { chapter: 4, name: '竜殺しの王', en: 'The Dragonkiller King' },
+  { chapter: 5, name: '平和王エドガー', en: 'Edgar the Peaceful' }
+]
