c349806dc7b6c924f6f6b7104ffe8c30197e075a
laineus <lainen000@gmail.com>
2019-07-27 18:24:30 +0900

    fix data fix design

--- src/data/chapters.js
+++ src/data/chapters.js
@@ -1,8 +1,8 @@
-export default {
-  0: { name: '序章 王殺しのジャック', en: 'Jack the Kingkiller' },
-  1: { name: '1章 ワルコフォレンスの森', en: 'Warcoforence' },
-  2: { name: '2章 トロイア公爵邸の地下通路', en: 'Troy\'s secret passage' },
-  3: { name: '3章 聖アンテルスの墓地', en: 'St Anterus\' catacomb' },
-  4: { name: '4章 グリファルデ神殿', en: 'Temple Grefalde' },
-  5: { name: '終章 平和王エドガー', en: 'Edgar the Peaceful' },
-}
+export default [
+  { name: '序章 王殺しのジャック', en: 'Jack the Kingkiller' },
+  { name: '1章 ワルコフォレンスの森', en: 'Warcoforence' },
+  { name: '2章 トロイア公爵邸の地下通路', en: 'Troy\'s secret passage' },
+  { name: '3章 聖アンテルスの墓地', en: 'St Anterus\' catacomb' },
+  { name: '4章 グリファルデ神殿', en: 'Temple Grefalde' },
+  { name: '終章 平和王エドガー', en: 'Edgar the Peaceful' },
+]
