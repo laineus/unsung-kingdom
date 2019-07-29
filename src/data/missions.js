fdfbdc1781985b3f97bd20e9d452ef826417d701
laineus <lainen000@gmail.com>
2019-07-29 22:11:14 +0900

    mission data

--- src/data/missions.js
+++ src/data/missions.js
@@ -1,22 +1,162 @@
 export default [
-  { chapter: 0, title: '王殺しのジャック', en: 'Jack the Kingkiller' },
-  { chapter: 1, title: 'マシューと狩猟犬', en: 'Matthew and Hunting dogs' },
-  { chapter: 1, title: 'アイザムバード傭兵団の討伐遠征', en: 'Isambard mercenaries' },
-  { chapter: 1, title: '森の賢人ドリスタン', en: 'Philosopher Drystan' },
-  { chapter: 1, title: 'ワルコフォレンスの人食い熊', en: 'The Rex Bear' },
-  { chapter: 2, title: 'メアリー王女と公爵令嬢ロレッタ', en: 'Princess Mary and Loretta' },
-  { chapter: 2, title: '泣くには名誉が足りぬ', en: 'Honor for tears' },
-  { chapter: 2, title: '幽閉されしカサンドラ', en: 'Incarcerated Cassandra' },
-  { chapter: 2, title: 'コルネリウス騎士団長の企て', en: 'The Dirty Knight' },
-  { chapter: 3, title: 'レンフィールド家の大罪', en: 'Sins of the Renfield family' },
-  { chapter: 3, title: 'ダリオの哀しみ', en: 'The Sorrow of Dario' },
-  { chapter: 3, title: 'マリアンヌの歌を聞いてから死ね', en: 'Listen Marianne\'s song before die' },
-  { chapter: 3, title: '呪われしフェルディナンド卿', en: 'Cursed Lord Ferdinand' },
-  { chapter: 3, title: 'エヴァンジェリナ妃の亡霊', en: 'The Ghost of Princess Evangelina' },
-  { chapter: 4, title: '不浄のロレイン像', en: 'Sinning Lorraine\'s statue' },
-  { chapter: 4, title: '女神さまに捧ぐリュート', en: 'Lute for goddess' },
-  { chapter: 4, title: 'すべて世は事もなし', en: 'All\'s right with the world' },
-  { chapter: 4, title: '聖剣カリブルヌス', en: 'Caliburnus' },
-  { chapter: 4, title: '竜殺しの王', en: 'The Dragonkiller King' },
-  { chapter: 5, title: '平和王エドガー', en: 'Edgar the Peaceful' }
+  {
+    key: '0_1',
+    chapter: 0,
+    title: '王殺しのジャック',
+    en: 'Jack the Kingkiller',
+    desc: '',
+    map: 'forest1'
+  },
+  {
+    key: '1_1',
+    chapter: 1,
+    title: 'マシューと狩猟犬',
+    en: 'Matthew and Hunting dogs',
+    desc: '',
+    map: 'forest2'
+  },
+  {
+    key: '1_2',
+    chapter: 1,
+    title: 'アイザムバード傭兵団の討伐遠征',
+    en: 'Isambard mercenaries',
+    desc: '',
+    map: 'forest5'
+  },
+  {
+    key: '1_3',
+    chapter: 1,
+    title: '森の賢人ドリスタン',
+    en: 'Philosopher Drystan',
+    desc: '',
+    map: 'forest9'
+  },
+  {
+    key: '1_4',
+    chapter: 1,
+    title: 'ワルコフォレンスの人食い熊',
+    en: 'The Rex Bear',
+    desc: '',
+    map: 'forest4'
+  },
+  {
+    key: '2_1',
+    chapter: 2,
+    title: 'メアリー王女と公爵令嬢ロレッタ',
+    en: 'Princess Mary and Loretta',
+    desc: '',
+    map: 'forest1'
+  },
+  {
+    key: '2_2',
+    chapter: 2,
+    title: '泣くには名誉が足りぬ',
+    en: 'Honor for tears',
+    desc: '',
+    map: 'forest1'
+  },
+  {
+    key: '2_3',
+    chapter: 2,
+    title: '幽閉されしカサンドラ',
+    en: 'Incarcerated Cassandra',
+    desc: '',
+    map: 'forest1'
+  },
+  {
+    key: '2_4',
+    chapter: 2,
+    title: 'コルネリウス騎士団長の企て',
+    en: 'The Dirty Knight',
+    desc: '',
+    map: 'forest1'
+  },
+  {
+    key: '3_1',
+    chapter: 3,
+    title: 'レンフィールド家の大罪',
+    en: 'Sins of the Renfield family',
+    desc: '',
+    map: 'forest1'
+  },
+  {
+    key: '3_2',
+    chapter: 3,
+    title: 'ダリオの哀しみ',
+    en: 'The Sorrow of Dario',
+    desc: '',
+    map: 'forest1'
+  },
+  {
+    key: '3_3',
+    chapter: 3,
+    title: 'マリアンヌの歌を聞いてから死ね',
+    en: 'Listen Marianne\'s song before die',
+    desc: '',
+    map: 'forest1'
+  },
+  {
+    key: '3_4',
+    chapter: 3,
+    title: '呪われしフェルディナンド卿',
+    en: 'Cursed Lord Ferdinand',
+    desc: '',
+    map: 'forest1'
+  },
+  {
+    key: '3_5',
+    chapter: 3,
+    title: 'エヴァンジェリナ妃の亡霊',
+    en: 'The Ghost of Princess Evangelina',
+    desc: '',
+    map: 'forest1'
+  },
+  {
+    key: '4_1',
+    chapter: 4,
+    title: '不浄のロレイン像',
+    en: 'Sinning Lorraine\'s statue',
+    desc: '',
+    map: 'forest1'
+  },
+  {
+    key: '4_2',
+    chapter: 4,
+    title: '女神さまに捧ぐリュート',
+    en: 'Lute for goddess',
+    desc: '',
+    map: 'forest1'
+  },
+  {
+    key: '4_3',
+    chapter: 4,
+    title: 'すべて世は事もなし',
+    en: 'All\'s right with the world',
+    desc: '',
+    map: 'forest1'
+  },
+  {
+    key: '4_4',
+    chapter: 4,
+    title: '聖剣カリブルヌス',
+    en: 'Caliburnus',
+    desc: '',
+    map: 'forest1'
+  },
+  {
+    key: '4_5',
+    chapter: 4,
+    title: '竜殺しの王',
+    en: 'The Dragonkiller King',
+    desc: '',
+    map: 'forest1'
+  },
+  {
+    key: '5_1',
+    chapter: 5,
+    title: '平和王エドガー',
+    en: 'Edgar the Peaceful',
+    desc: '',
+    map: 'forest1'
+  }
 ]
