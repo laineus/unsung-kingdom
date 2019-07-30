89bf3c2f520cca4e72455929a4aa9b996bc69e3d
laineus <lainen000@gmail.com>
2019-07-31 00:10:03 +0900

    content

--- src/class/MenuSave.js
+++ src/class/MenuSave.js
@@ -27,22 +27,6 @@ export default class MenuSave extends Phaser.GameObjects.Container {
       const time = this.scene.add.text(105, 0, timeString, { fontSize: 12, fontFamily: config.FONT }).setOrigin(1, 0.5)
       item.add(time)
     }
-    const save = this.scene.add.text(100, 0, 'save', { align: 'center', fontSize: 16, fontStyle: 'bold', fontFamily: config.FONT })
-    save.setInteractive().on('pointerdown', () => {
-      this.scene.storage.save(data.number)
-    })
-    item.add(save)
-    if (data.exists) {
-      const load = this.scene.add.text(140, 0, 'load', { align: 'center', fontSize: 17, fontStyle: 'bold', fontFamily: config.FONT })
-      const detail = this.scene.add.text(200, 0, `map: ${data.state.map} x: ${data.state.x} y: ${data.state.y}`, { align: 'center', fontSize: 15, fontFamily: config.FONT })
-      load.setInteractive().on('pointerdown', () => {
-        this.scene.storage.load(data.number)
-        this.scene.gameScene.mapChange(data.state.map, data.state.x, data.state.y).then(() => {
-          this.emit('close')
-        })
-      })
-      item.add([load, detail])
-    }
     return item
   }
   setContent (data) {
@@ -54,6 +38,22 @@ export default class MenuSave extends Phaser.GameObjects.Container {
     const container = this.scene.add.container(x, y)
     const tx = this.scene.add.text(0, 0, `Data ${data.number}`, { fontSize: 17, fontStyle: 'bold', fontFamily: config.FONT })
     container.add(tx)
+    const save = this.scene.add.text(100, 0, 'save', { align: 'center', fontSize: 16, fontStyle: 'bold', fontFamily: config.FONT })
+    save.setInteractive().on('pointerdown', () => {
+      this.scene.storage.save(data.number)
+    })
+    container.add(save)
+    if (data.exists) {
+      const load = this.scene.add.text(140, 0, 'load', { align: 'center', fontSize: 17, fontStyle: 'bold', fontFamily: config.FONT })
+      const detail = this.scene.add.text(200, 0, `map: ${data.state.map} x: ${data.state.x} y: ${data.state.y}`, { align: 'center', fontSize: 15, fontFamily: config.FONT })
+      load.setInteractive().on('pointerdown', () => {
+        this.scene.storage.load(data.number)
+        this.scene.gameScene.mapChange(data.state.map, data.state.x, data.state.y).then(() => {
+          this.emit('close')
+        })
+      })
+      container.add([load, detail])
+    }
     return container
   }
 }
