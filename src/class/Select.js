a576f8f9a29d2ff88dbcb744d9236db1a35c669a
laineus <lainen000@gmail.com>
2019-08-03 19:17:27 +0900

    select

--- src/class/Select.js
+++ src/class/Select.js
@@ -1,7 +1,7 @@
-import config from '../data/config'
-const WIDTH = 300
-const HEIGHT = 44
-const MERGIN = 6
+import Button from './Button'
+const WIDTH = 240
+const HEIGHT = 40
+const MERGIN = 10
 export default class Select extends Phaser.GameObjects.Container {
   constructor (scene, options, callback) {
     super(scene)
@@ -10,26 +10,18 @@ export default class Select extends Phaser.GameObjects.Container {
     scene.add.existing(this)
     scene.scene.pause('Game')
     this.options = options.map((text, i) => {
-      const opt = this.option(text, i).setPosition(0, i * (HEIGHT + MERGIN))
-      this.add(opt)
+      this.add(this.option(text, i, 0, i * (HEIGHT + MERGIN)))
     })
-    this.setPosition((20).byRight - WIDTH, (20).byBottom - (config.HEIGHT / 4) - this.sumHeight - MERGIN)
+    this.setPosition((20).byRight - WIDTH.half, (20).byBottom - this.yDiff)
   }
-  option (text, i) {
-    const option = this.scene.add.container(0, 0)
-    const window = this.scene.add.rectangle(0, 0, WIDTH, HEIGHT, 0x000000, 100).setOrigin(0, 0).setInteractive()
-    const label = this.scene.add.text(18, 13, text, { fontSize: 18, lineSpacing: 13 }).setPadding(0, 2, 0, 0)
-    option.add([window, label])
-    window.on('pointerover', () => label.setColor('#3A8'))
-    window.on('pointerout', () => label.setColor('#FFF'))
-    window.on('pointerdown', pointer => {
+  option (text, i, x, y) {
+    return new Button(this.scene, x, y, text, WIDTH, HEIGHT).on('click', () => {
       this.scene.scene.resume('Game')
       this.destroy()
       if (this.callback) this.callback(i)
     })
-    return option
   }
-  get sumHeight () {
-    return (this.options.length * HEIGHT) + ((this.options.length - 1) * MERGIN)
+  get yDiff () {
+    return ((this.options.length * HEIGHT) - HEIGHT.half) + ((this.options.length - 1) * MERGIN)
   }
 }
