457cdb12f12a65302ef87731284b3c5e52831f50
laineus <lainen000@gmail.com>
2019-08-03 19:17:38 +0900

    button align

--- src/class/Button.js
+++ src/class/Button.js
@@ -1,13 +1,15 @@
 import Box from './Box'
 import config from '../data/config'
 export default class Button extends Phaser.GameObjects.Container {
-  constructor (scene, x, y, text, width = 120, height = 80, option = {}) {
+  constructor (scene, x, y, text, width = 120, height = 80, option = { align: 'center' }) {
     super(scene, x, y)
+    const { align } = option
     this.setSize(width, height).setInteractive()
     this.bgLight = new Box(scene, -4, 4, width, height, { color: 0xAACC22, alpha: 1 }).setOrigin(0.5, 0.5)
     this.bgLight.visible = false
     const bg = new Box(scene, 0, 0, width, height, { color: 0x000000, lineColor: 0xffffff }).setOrigin(0.5, 0.5)
-    const tx = scene.add.text(0, 0, text, { align: 'center', fontFamily: config.FONT }).setOrigin(0.5, 0.5).setPadding(0, 2, 0, 0)
+    const alignLeft = align === 'left'
+    const tx = scene.add.text(alignLeft ? -width.half + 20 : 0, 0, text, { align, fontFamily: config.FONT }).setOrigin(alignLeft ? 0 : 0.5, 0.5).setPadding(0, 2, 0, 0)
     this.add([this.bgLight, bg, tx])
     this.on('pointerover', () => {
       this.bgLight.visible = true
