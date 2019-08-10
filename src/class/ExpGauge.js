746212762646e393910c0411674cb0edb4af8ffc
laineus <lainen000@gmail.com>
2019-08-10 16:06:35 +0900

    fix gauge alpha

--- src/class/ExpGauge.js
+++ src/class/ExpGauge.js
@@ -8,10 +8,9 @@ export default class ExpGauge extends Phaser.GameObjects.Container {
     this.exp = exp
     scene.add.existing(this)
     this.setPosition(x, y)
-    const height = 3
     this.bg = scene.add.rectangle(-width.half, 0, width, 1, config.COLORS.soy).setOrigin(0, 0.5)
-    this.bar = scene.add.rectangle(-width.half, 0, width - 9, height, config.COLORS.soy).setOrigin(0, 1).setScale(this.barScale, 1)
-    this.arrow = scene.add.polygon(width.half + 1, 5, [[0, -5], [0, 0], [10, 0]], config.COLORS.soy).setOrigin(1, 1)
+    this.bar = scene.add.rectangle(-width.half, -1, width - 11, 2, config.COLORS.soy).setOrigin(0, 1).setScale(this.barScale, 1)
+    this.arrow = scene.add.polygon(width.half -1, 4, [[0, -5], [0, 0], [10, 0]], config.COLORS.soy).setOrigin(1, 1)
     this.label = this.scene.add.text(-width.half, -4, 'EXP', { fill: config.COLORS.soy.toColorString, fontSize: 9, fontStyle: 'bold', fontFamily: config.FONT }).setOrigin(0, 1)
     this.add([this.bg, this.bar, this.arrow, this.label])
   }
