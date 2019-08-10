e1081c3625f1bf8ee31a3b7af0d1fe2e7102788f
laineus <lainen000@gmail.com>
2019-08-10 13:41:01 +0900

    lv up

--- src/class/BattleResult.js
+++ src/class/BattleResult.js
@@ -9,18 +9,23 @@ export default class Battle extends Phaser.GameObjects.Container {
     scene.add.existing(this)
     this.scene = scene
     this.group = group
-    this.increaceExp()
-    this.levelUpPlayers()
     const b = new Box(this.scene, -110, 0, 450, config.HEIGHT).setOrigin(0, 0)
     this.add(b)
-    storage.state.battlers.map((v, i) => this.getChara(v, 120, 100 + i * 60))
+    this.charas = storage.state.battlers.map((v, i) => this.getChara(v, 120, 100 + i * 60))
+    this.increaceExp()
   }
   increaceExp () {
     const sumExp = this.group.reduce((before, current) => (before + current.lv * 3), 0)
     const alives = storage.state.battlers.filter(v => v.hp > 0)
-    alives.forEach(v => {
-      v.exp += sumExp / alives.length
+    const eachExp = sumExp / alives.length
+    this.charas.filter(v => v.data.hp > 0).forEach(v => {
+      v.gauge.addExp(eachExp)
+      v.gauge.on('lvUp', v.lvUp)
     })
+    alives.forEach(v => {
+      v.exp += eachExp
+    })
+    this.levelUpPlayers()
   }
   levelUpPlayers () {
     const levelUp = battler => {
@@ -37,13 +42,23 @@ export default class Battle extends Phaser.GameObjects.Container {
   }
   getChara (chara, x, y) {
     const container = this.scene.add.container(x, y)
+    container.data = chara
     const sprite = this.scene.add.sprite(-110, 0, chara.key).setScale(0.25).setOrigin(0, 0)
     sprite.setCrop(0, 0, sprite.width, 150)
     const name = this.scene.add.text(-53, 0, chara.name, { fill: config.COLORS.theme.toColorString, stroke: config.COLORS.dark.toColorString, strokeThickness: 2, fontSize: 14, fontStyle: 'bold', fontFamily: config.FONT })
-    const lv = this.scene.add.text(130, 3, `Lv ${chara.lv}`, { fill: config.COLORS.theme.toColorString, stroke: config.COLORS.dark.toColorString, strokeThickness: 2, fontSize: 12, fontStyle: 'bold', fontFamily: config.FONT, align: 'right' }).setOrigin(1, 0)
-    const gauge = new ExpGauge(this.scene, 38, 38, 180, 9, 120)
-    gauge.addExp(50)
-    gauge.on('lvUp', (e) => console.log(e))
+    const lv = this.scene.add.text(130, 15, `Lv ${chara.lv}`, { fill: config.COLORS.theme.toColorString, stroke: config.COLORS.dark.toColorString, strokeThickness: 2, fontSize: 12, fontStyle: 'bold', fontFamily: config.FONT, align: 'right' }).setOrigin(1, 0.5)
+    const gauge = new ExpGauge(this.scene, 38, 38, 180, chara.lv, chara.exp)
+    container.lvUp = newLv => {
+      lv.text = `Lv ${newLv}`
+      this.scene.add.tween({
+        targets: lv,
+        duration: 100,
+        ease: 'Power2',
+        scale: 1.3,
+        yoyo: true
+      })
+    }
+    container.gauge = gauge
     container.add([sprite, gauge, name, lv])
     return container
   }
