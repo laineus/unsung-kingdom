8cf9518cdc340cdaed767a17f84526c23b5d1915
laineus <lainen000@gmail.com>
2019-08-10 13:21:51 +0900

    Result

--- src/class/BattleResult.js
+++ src/class/BattleResult.js
@@ -6,16 +6,14 @@ import expTable from '../data/expTable'
 export default class Battle extends Phaser.GameObjects.Container {
   constructor (scene, group, callback) {
     super(scene)
+    scene.add.existing(this)
     this.scene = scene
     this.group = group
     this.increaceExp()
     this.levelUpPlayers()
     const b = new Box(this.scene, -110, 0, 450, config.HEIGHT).setOrigin(0, 0)
-    this.scene.add.existing(b)
-    const g = new ExpGauge(this.scene, 120, 100, 180, 9, 120)
-    g.addExp(50)
-    g.on('lvUp', (e) => console.log(e))
-    this.scene.add.existing(g)
+    this.add(b)
+    storage.state.battlers.map((v, i) => this.getChara(v, 120, 100 + i * 60))
   }
   increaceExp () {
     const sumExp = this.group.reduce((before, current) => (before + current.lv * 3), 0)
@@ -36,5 +34,17 @@ export default class Battle extends Phaser.GameObjects.Container {
       }
     }
     storage.state.battlers.filter(v => v.hp > 0).forEach(v => levelUp(v))
+  }
+  getChara (chara, x, y) {
+    const container = this.scene.add.container(x, y)
+    const sprite = this.scene.add.sprite(-110, 0, chara.key).setScale(0.25).setOrigin(0, 0)
+    sprite.setCrop(0, 0, sprite.width, 150)
+    const name = this.scene.add.text(-53, 0, chara.name, { fill: config.COLORS.theme.toColorString, stroke: config.COLORS.dark.toColorString, strokeThickness: 2, fontSize: 14, fontStyle: 'bold', fontFamily: config.FONT })
+    const lv = this.scene.add.text(130, 3, `Lv ${chara.lv}`, { fill: config.COLORS.theme.toColorString, stroke: config.COLORS.dark.toColorString, strokeThickness: 2, fontSize: 12, fontStyle: 'bold', fontFamily: config.FONT, align: 'right' }).setOrigin(1, 0)
+    const gauge = new ExpGauge(this.scene, 38, 38, 180, 9, 120)
+    gauge.addExp(50)
+    gauge.on('lvUp', (e) => console.log(e))
+    container.add([sprite, gauge, name, lv])
+    return container
   }
 }
