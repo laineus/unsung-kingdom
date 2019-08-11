df010ad17e848e2fde6ff19ab5d83f4f79d35813
laineus <lainen000@gmail.com>
2019-08-11 21:48:52 +0900

    sample weapons

--- src/data/weapons.js
+++ src/data/weapons.js
@@ -10,5 +10,14 @@
 //   4: counter
 export default [
   { id: 1, type: 1, name: 'ショートソード', atk: 5, ability: 4 },
-  { id: 2, type: 1, name: 'ロングソード', atk: 7, ability: 1 }
+  { id: 2, type: 1, name: 'カトラス', atk: 5, ability: 4 },
+  { id: 3, type: 1, name: 'グラディウス', atk: 8, ability: null },
+  { id: 4, type: 1, name: 'ロングソード', atk: 7, ability: 1 },
+  { id: 5, type: 1, name: 'バスタードソード', atk: 8, ability: null },
+  { id: 6, type: 1, name: 'ツヴァイハンダー', atk: 8, ability: null },
+  { id: 7, type: 1, name: 'クレイモア', atk: 8, ability: null },
+  { id: 8, type: 1, name: 'ファルシオン', atk: 8, ability: null },
+  { id: 9, type: 1, name: 'フランベルジュ', atk: 8, ability: null },
+  { id: 10, type: 1, name: 'カタナ', atk: 8, ability: null },
+  { id: 11, type: 1, name: 'カリブルヌス', atk: 8, ability: null }
 ]
