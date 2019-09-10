acd088981b8e2e36a55bce4432581c853d25a7fb
laineus <lainen000@gmail.com>
2019-09-10 23:57:54 +0900

    increase battler status

--- src/util/generateBattler.js
+++ src/util/generateBattler.js
@@ -1,13 +1,13 @@
 import battlers from '../data/battlers'
-export default (key, lv) => {
+export default (key, lv, increase = {}) => {
   const base = battlers.find(v => v.key === key)
   const addTimes = lv - base.lv
   return Object.assign(Object.assign({}, base), {
     lv,
-    hp: base.hp + Math.floor(base.up.hp * addTimes / 20),
-    atk: base.atk + Math.floor(base.up.atk * addTimes / 100),
-    def: base.def + Math.floor(base.up.def * addTimes / 100),
-    dex: base.dex + Math.floor(base.up.dex * addTimes / 100),
-    agi: base.agi + Math.floor(base.up.agi * addTimes / 100)
+    hp: base.hp + Math.floor(base.up.hp * addTimes / 20) + (increase.hp || 0),
+    atk: base.atk + Math.floor(base.up.atk * addTimes / 100) + (increase.atk || 0),
+    def: base.def + Math.floor(base.up.def * addTimes / 100) + (increase.def || 0),
+    dex: base.dex + Math.floor(base.up.dex * addTimes / 100) + (increase.dex || 0),
+    agi: base.agi + Math.floor(base.up.agi * addTimes / 100) + (increase.agi || 0)
   })
 }
