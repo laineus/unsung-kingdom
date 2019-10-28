8a1f616042ba704eb55aefc5b1eca111f872f60a
laineus <lainen000@gmail.com>
2019-10-29 00:52:01 +0900

    fix gate

--- src/class/Gate.js
+++ src/class/Gate.js
@@ -3,7 +3,7 @@ export default class Gate extends Area {
   constructor (scene, key, x, y, zone_x, zone_y, zone_width, zone_height) {
     super(scene, zone_x, zone_y, zone_width, zone_height)
     this.originalEvent = () => {
-      scene.mapChange(key, x.toPixelCenter, y.toPixelCenter)
+      key === 'world' ? scene.ui.worldMap() : scene.mapChange(key, x.toPixelCenter, y.toPixelCenter)
     }
     this.setEvent(this.originalEvent)
   }
