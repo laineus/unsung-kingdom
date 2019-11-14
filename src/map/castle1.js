2a35c060f5951825aa1774d6eceecfb17982e603
laineus <lainen000@gmail.com>
2019-11-15 01:04:11 +0900

    castle event

--- src/map/castle1.js
+++ src/map/castle1.js
@@ -5,14 +5,24 @@ export default {
     x: 0, y: 0
   },
   create (scene) {
+    const jack = scene.map.getObjectById(10)
+    const king = scene.map.getObjectById(11)
+    jack.setVisible(false)
+    king.setVisible(false)
     scene.map.getObjectById(8).setEvent(async () => {
       scene.setEventMode(true)
       scene.player.stopWalk()
       await scene.ui.sleep(500)
       await scene.ui.transition(false, 'slow')
-      scene.player.setPosition((49).toPixel, (29).toPixel)
+      scene.player.setPosition((50).toPixel, (29).toPixel)
       scene.camera.updateFollow()
       await scene.ui.sleep(500)
+      scene.camera.followPlayer(false)
+      await scene.camera.move(-10, -200, 1000)
+      await scene.ui.sleep(2500)
+      scene.camera.followPlayer(true, true)
+      console.log(1)
+      scene.setEventMode(false)
     })
   }
 }
