634590867634de0c3a725b78cb141156942ed65d
laineus <lainen000@gmail.com>
2019-11-23 21:41:43 +0900

    add people

--- src/map/town1.js
+++ src/map/town1.js
@@ -14,7 +14,9 @@ export default {
     const amber = scene.map.getObjectById(2).setDisplayName('アンバー婦人').setTapEvent()
     const elliott = scene.map.getObjectById(7).setDisplayName('卑劣なエリオット').setTapEvent()
     const max = scene.map.getObjectById(8).setDisplayName('傭兵マックス').setTapEvent()
-    const matilda = scene.map.getObjectById(9).setDisplayName('宿屋のマチルダ').setTapEvent()
-    events[scene.storage.state.chapter](scene, { amber, elliott, max, matilda })
+    const annabelle = scene.map.getObjectById(9).setDisplayName('宿屋のアナベル').setTapEvent()
+    const matilda = scene.map.getObjectById(10).setDisplayName('マチルダ').setTapEvent()
+    const maison = scene.map.getObjectById(11).setDisplayName('メイソン').setTapEvent()
+    events[scene.storage.state.chapter](scene, { amber, elliott, max, annabelle, matilda, maison })
   }
 }
