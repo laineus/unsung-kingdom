940f365f1e92da6b3e2b1c76ad27f6e9a6e0abcb
laineus <lainen000@gmail.com>
2019-11-24 17:46:58 +0900

    bugfix

--- src/class/BattleQuestService.js
+++ src/class/BattleQuestService.js
@@ -1,21 +1,21 @@
 import storage from '../data/storage'
 import { MANDRAKE_COUNT } from '../event/drystan'
-const event = storage.state.event
 export default class BattleQuestService {
   constructor (group) {
     this.group = group
   }
   getResult () {
-    const list = []
-    list.push(this.m1_3())
-    return list.filter(v => v)
+    const list = [
+      this.m1_3
+    ]
+    return list.map(v => v.call(this, storage.state.event)).filter(Boolean)
   }
   collection (target, chance = 100) {
     return this.group.filter(v => {
       return v.key === target && Math.chance(chance)
     }).length
   }
-  m1_3 () {
+  m1_3 (event) {
     if (event.m1_3.completed || !event.m1_3.started) return
     const result = this.collection('mandrake', 66)
     if (!result) return
