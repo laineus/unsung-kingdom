117a0016bef4abd067441728d798d86b9b6f649b
laineus <lainen000@gmail.com>
2019-11-25 23:56:56 +0900

    delete unnecessary

--- src/data/config.js
+++ src/data/config.js
@@ -1,8 +1,6 @@
 export default {
   WIDTH: 960,
-  get WIDTH_HALF () { return this.WIDTH / 2 },
   HEIGHT: 540,
-  get HEIGHT_HALF () { return this.HEIGHT / 2 },
   TILE_SIZE: 32,
   FONTS: {
     UI: '"Teko"',
