e3b5267031c4c9291ee92ed810f95954d0423087
laineus <lainen000@gmail.com>
2019-11-29 00:39:55 +0900

    refactoring

--- src/util/increaseWeapon.js
+++ src/util/increaseWeapon.js
@@ -3,7 +3,7 @@ import storage from '../data/storage'
 export default weaponId => {
   const weapon = weapons.find(v => v.id === weaponId)
   if (!weapon) return false
-  const id = storage.state.weapons.reduce((prev, current) => Math.max(prev, current.id), 0) + 1
+  const id = Math.max(...storage.state.weapons.map(v => v.id)) + 1
   storage.state.weapons.push({ id, weapon_id: weapon.id })
   return weapon
 }
