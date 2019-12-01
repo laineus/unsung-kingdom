4eaa6165655ecf2b93c4279ea973ecfae6d89f70
laineus <lainen000@gmail.com>
2019-12-01 16:56:19 +0900

    config

--- src/class/Menu.js
+++ src/class/Menu.js
@@ -5,9 +5,10 @@ import MenuSave from './MenuSave'
 import { slideIn, slideOut, fadeIn, fadeOut } from '../util/animations'
 import UICloseButton from './UICloseButton'
 const contents = [
-  { class: MenuMap, name: 'MAP & QUEST', min: 'マップ・クエスト', frame: 0 },
-  { class: MenuStatus, name: 'CHARACTER', min: 'キャラクター', frame: 1 },
-  { class: MenuSave, name: 'SAVE', min: 'セーブ', frame: 2 }
+  { class: MenuMap, name: 'MAP & QUEST', min: 'マップ・クエスト', size: 52, frame: 0 },
+  { class: MenuStatus, name: 'CHARACTER', min: 'キャラクター', size: 52, frame: 1 },
+  { class: MenuSave, name: 'SAVE', min: 'セーブ', size: 52, frame: 2 },
+  { class: MenuMap, name: 'CONFIG', min: '設定', size: 42, frame: 3 }
 ]
 export default class Menu extends Phaser.GameObjects.Container {
   constructor (scene, callback) {
@@ -19,7 +20,11 @@ export default class Menu extends Phaser.GameObjects.Container {
     this.bg.blendMode = 1
     this.window = scene.add.polygon(0, 0, [[0, 0], [(50).byRight, 0], [(150).byRight, (0).byBottom], [0, (0).byBottom]], config.COLORS.black, 0.7).setOrigin(0, 0)
     this.add([this.bg, this.window])
-    this.buttons = contents.map((content, i) => this.button(content, (15).byRight, i * 125 + 15))
+    this.buttons = contents.map((content, i) => {
+      const y = contents.slice(0, i).reduce((sum, v) => sum + (v.size * 2) + 10, 15) + content.size
+      const x = Math.max(...contents.map(v => v.size)) + 15
+      return this.button(content, x.byRight, y)
+    })
     this.add(this.buttons)
     this.close = new UICloseButton(scene, (70).byRight, (35).byBottom).on('click', this.destroy.bind(this, true))
     this.add(this.close)
@@ -42,11 +47,11 @@ export default class Menu extends Phaser.GameObjects.Container {
     })
   }
   button (content, x, y) {
-    const button = this.scene.add.container(x - 50, y + 50).setSize(100, 100)
-    const bg = this.scene.add.circle(0, 0, 55, config.COLORS.black)
-    const icon = this.scene.add.sprite(0, -7, 'menu_icons').setOrigin(0.5, 0.5).setTint(config.COLORS.ghost).setFrame(content.frame)
-    const tx = this.scene.add.text(0, 31, content.name, { align: 'center', fill: config.COLORS.theme.toColorString, fontSize: 20, fontStyle: 'bold', fontFamily: config.FONTS.UI }).setOrigin(0.5, 1).setPadding(0, 2, 0, 0)
-    const min = this.scene.add.text(0, 27, content.min, { align: 'center', fill: config.COLORS.gray.toColorString, fontSize: 9, fontStyle: 'bold', fontFamily: config.FONTS.TEXT }).setOrigin(0.5, 0).setPadding(0, 2, 0, 0)
+    const button = this.scene.add.container(x, y).setSize(104, 104)
+    const bg = this.scene.add.circle(0, 0, content.size, config.COLORS.black)
+    const icon = this.scene.add.sprite(0, -8, 'menu_icons').setOrigin(0.5, 0.5).setTint(config.COLORS.ghost).setFrame(content.frame)
+    const tx = this.scene.add.text(0, content.size - 24, content.name, { align: 'center', fill: config.COLORS.theme.toColorString, fontSize: 20, fontStyle: 'bold', fontFamily: config.FONTS.UI }).setOrigin(0.5, 1).setPadding(0, 2, 0, 0)
+    const min = this.scene.add.text(0, content.size - 28, content.min, { align: 'center', fill: config.COLORS.gray.toColorString, fontSize: 10, fontStyle: 'bold', fontFamily: config.FONTS.TEXT }).setOrigin(0.5, 0).setPadding(0, 2, 0, 0)
     button.add([bg, icon, tx, min])
     button.setInteractive().on('pointerdown', this.loadContent.bind(this, content))
     return button
