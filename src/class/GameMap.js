c02ccd0236bc22322731b5cadda05c024edae8e9
laineus <lainen000@gmail.com>
2019-12-04 23:21:03 +0900

    treasure angle

--- src/class/GameMap.js
+++ src/class/GameMap.js
@@ -23,7 +23,7 @@ export default class GameMap {
     this.areas = this._getObjects(tilemap, 'area').map(this._toAreaData).map(area => new Area(scene, area.zone_x, area.zone_y, area.zone_width, area.zone_height).setId(area.id))
     this.charas = this._getObjects(tilemap, 'chara').map(data => new Character(scene, data.x, data.y, data.name).setR((data.rotation + 90) * (Math.PI / 180)).setId(data.id))
     this.objects = this._getObjects(tilemap, 'object').map(data => new Substance(scene, data.x, data.y, data.name).setId(data.id))
-    this.treasures = this._getObjects(tilemap, 'treasure').map(data => new TreasureChest(scene, data.x, data.y, Number(data.name), `${mapKey}_${data.id}`).setId(data.id))
+    this.treasures = this._getObjects(tilemap, 'treasure').map(data => new TreasureChest(scene, data.x, data.y, Number(data.name), `${mapKey}_${data.id}`, data.rotation === 90).setId(data.id))
     this.images = tilemap.images.map(data => scene.add.sprite(data.x, data.y, `tileset/${data.name}`).setOrigin(0, 0))
     // this.scene.ui.renderMiniMap(tilemap)
     return this
--- src/class/TreasureChest.js
+++ src/class/TreasureChest.js
@@ -1,8 +1,9 @@
 import Substance from './Substance'
 import increaseWeapon from '../util/increaseWeapon'
 export default class TreasureChest extends Substance {
-  constructor (scene, x, y, weaponId, stateKey) {
+  constructor (scene, x, y, weaponId, stateKey, left) {
     super(scene, x, y, 'treasure_chest')
+    if (left) this.image.setScale(-1, 1)
     const treasures = scene.storage.state.treasures
     if (treasures.includes(stateKey)) return
     this.setTapEvent(async () => {
