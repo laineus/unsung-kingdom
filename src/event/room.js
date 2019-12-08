7c506a6fe748f9ab3ca05b97273cef9b4cfb94bb
laineus <lainen000@gmail.com>
2019-12-08 18:31:09 +0900

    fix deafult weapon

--- src/event/room.js
+++ src/event/room.js
@@ -2,7 +2,7 @@ import chapters from '../data/chapters'
 
 const setDefaultWeapon = state => {
   state.weapons = [{ id: 1, weapon_id: 1 }, { id: 2, weapon_id: 1 }, { id: 3, weapon_id: 2 }]
-  state.battlers.forEach((v, i) => Object.assign(v, { exp: 0, maxHp: v.hp, weapon: i + 1 }))
+  state.battlers.forEach((v, i) => (v.weapon = i + 1))
 }
 
 export const execChapterBeginEvents = (scene) => {
