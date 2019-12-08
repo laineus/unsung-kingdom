482b582da5a82c7f6c8bec99949b00b4ebe04b55
laineus <lainen000@gmail.com>
2019-12-08 18:23:28 +0900

    fix mission desc

--- src/data/missionDescriptions.js
+++ src/data/missionDescriptions.js
@@ -15,11 +15,14 @@ export default {
     return `${base}\n${cnt}`
   },
   m1_2 (state) {
+    return 'アイザムバード傭兵団の負傷兵が行方不明になっている。\n付近を捜索しよう。'
+  },
+  m1_3 (state) {
     const base = '薬の材料となるマンドレイクの根を集めてドリスタンに届ける。'
     const cnt = `収集したマンドレイク（${state.event.m1_3.count}/${MANDRAKE_COUNT}）`
     return `${base}\n${cnt}`
   },
-  m1_3 (state) {
+  m1_4 (state) {
     const base = '凶暴なレックスベアを倒し、薬の材料となるレックスベアの血液をドリスタンに届ける。'
     const bear = '森の深くに棲むレックスベアを探し出して倒そう。'
     const solved = 'レックスベアの血液を手に入れた。ドリスタンの元へ届けよう。'
