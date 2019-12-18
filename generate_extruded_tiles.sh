c6b4d2db1bd1a3b1516d0415c872fe6b4f80905d
laineus <lainen000@gmail.com>
2019-12-19 00:16:21 +0900

    delete shell

--- generate_extruded_tiles.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for file in `\find public/img/tileset/original/*.png -maxdepth 1 -type f`; do
-  cp $file ${file/original/extruded}
-  npx tile-extruder --tileWidth 32 --tileHeight 32 --margin 0 --spacing 0 --input $file --output ${file/original/extruded}
-done
