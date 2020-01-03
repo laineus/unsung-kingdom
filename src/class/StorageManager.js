ebff0b229bf197be661943815c6eae29e24469f7
laineus <lainen000@gmail.com>
2020-01-03 17:43:49 +0900

    increace save slot

--- src/class/StorageManager.js
+++ src/class/StorageManager.js
@@ -14,7 +14,7 @@ export default class StorageManager {
     this.state = state
   }
   getList () {
-    return (6).toArray.map(this.getRow.bind(this))
+    return (9).toArray.map(this.getRow.bind(this))
   }
   getRow (number) {
     const state = this.getSavedState(number)
