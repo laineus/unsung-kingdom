8b7e902f930ad93de99af90b4d305171fedd28a7
laineus <lainen000@gmail.com>
2020-01-17 00:11:38 +0900

    AssetPlugin settings

--- AssetPlugin.js
+++ AssetPlugin.js
@@ -1,12 +1,26 @@
 const fs = require('fs')
-const DIR = './public/img/face'
+
+const ASSET_SETTINGS = require('./assetSettings')
 
 module.exports = class {
   apply (compiler) {
     compiler.hooks.beforeCompile.tapAsync('Asset', (_, callback) => {
-      fs.readdir(DIR, (_, files) => {
-        const fileList = files.filter(file => (/.*\.png$/).test(file))
-        compiler.options.externals.fileList = JSON.stringify(fileList)
+      const promises = ASSET_SETTINGS.map(setting => {
+        return new Promise(resolve => {
+          fs.readdir(`./public/${setting.dir}`, (_, files) => {
+            const list = files.filter(file => setting.rule.test(file)).map(file => {
+              return [`${setting.prefix}/${file.split('.')[0]}`, `..${setting.dir}/${file}`]
+            })
+            resolve({ prefix: setting.prefix, list })
+          })
+        })
+      })
+      Promise.all(promises).then(results => {
+        const object = results.reduce((obj, v) => {
+          obj[v.prefix] = v.list
+          return obj
+        }, {})
+        compiler.options.externals.assetData = JSON.stringify(object)
         callback()
       })
     })
---
+++ assetSettings.js
@@ -0,0 +1,4 @@
+module.exports = [
+  { prefix: 'face', dir: '/img/face', rule: /.*\.png$/ },
+  { prefix: 'battler', dir: '/img/battler', rule: /.*\.png$/ }
+]
