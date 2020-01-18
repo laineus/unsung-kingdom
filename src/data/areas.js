c055f8d3f2333dcb0ba5ffccbb7505ad51f9a39c
laineus <lainen000@gmail.com>
2020-01-19 03:15:37 +0900

    areas

---
+++ src/data/areas.js
@@ -0,0 +1,16 @@
+export default [
+  {
+    key: 'forest',
+    list: [
+      { key: 'forest1', x: 103, y: 51 },
+      { key: 'forest2', x: 74, y: 83 },
+      { key: 'forest3', x: 71, y: 35 },
+      { key: 'forest4', x: 44, y: 3 },
+      { key: 'forest5', x: 55, y: 115 },
+      { key: 'forest6', x: 22, y: 83 },
+      { key: 'forest7', x: 39, y: 35 },
+      { key: 'forest8', x: 7, y: 32 },
+      { key: 'forest9', x: 0, y: 0 }
+    ]
+  }
+]
