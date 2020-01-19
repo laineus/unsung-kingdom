7a2cec10bd57e3dbb8ad3efe16c4587caa66d1b2
laineus <lainen000@gmail.com>
2020-01-19 15:25:25 +0900

    show only visited fields

--- src/class/FieldMap.js
+++ src/class/FieldMap.js
@@ -5,7 +5,8 @@ export default class FieldMap extends Phaser.GameObjects.Container {
     super(scene)
     this.key = key
     const area = areas.find(v => v.key === key)
-    const sprites = area.list.map(v => {
+    const visited = scene.storage.state.visited
+    const sprites = area.list.filter(v => visited.includes(v.key)).map(v => {
       const x = v.x * TILE
       const y = v.y * TILE
       return scene.add.sprite(x, y, `map/${v.key}`).setOrigin(0, 0)
