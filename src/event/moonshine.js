fb66aff4f25dea76f52c36c52e554cd8c248e44a
laineus <lainen000@gmail.com>
2020-01-24 23:52:04 +0900

    improve scripts for dionysus

--- src/event/moonshine.js
+++ src/event/moonshine.js
@@ -12,21 +12,27 @@ export const dionysus = (scene, dionysus, area, gate) => {
   area.setEvent(async () => {
     await scene.talk([
       { chara, text: 'おい、本気で行く気かよ？' },
-      { chara, text: 'それなら俺にも案があるんだ。' },
+      { chara, text: 'やめとけって。' },
+      { chara, text: '死んじまうぞ？' },
+      { chara: 'ann', text: 'そう言われても。' },
+      { chara, text: '若い女の死体なんか見たくねえぞ。' },
+      { chara: 'ann', text: 'じゃあどうすればいいの？' },
+      { chara, text: 'うーん…。' },
+      { chara, text: 'よし、ちょっと危険だが案がある。' },
       { chara, text: '協力しようじゃないか。' },
       { chara: 'ann', text: '案？' },
       { chara, text: 'ああ。' },
       { chara, text: '俺がオルトロスの野郎を「倒しやすく」してやる。' },
       { chara: 'ann', text: 'どうやって？' },
       { chara, text: 'まあそれは任せとけ。' },
-      { chara, text: 'だが必ず倒せよ。俺も身を削るんだ。' },
+      { chara, text: 'だが必ず倒せよ。俺も身を削るんだからな。' },
       { chara, text: 'よし、じゃあちょっと待ってろ。' }
     ])
     await dionysus.setTargetPosition((8).toPixelCenter, (0).toPixelCenter)
     dionysus.setVisible(false)
     await scene.ui.sleep(500)
     await scene.talk([
-      { chara: 'jaquelyn', text: '勝手に行っちゃったわ。' },
+      { chara: 'jaquelyn', text: '行っちゃったわ。' },
       { chara: 'ann', text: '酒樽担いで行ったけど、どうするつもりだろう？' }
     ])
     await scene.ui.transition('slow')
@@ -35,10 +41,10 @@ export const dionysus = (scene, dionysus, area, gate) => {
     await scene.ui.sleep(500)
     await dionysus.setTargetPosition(origin.x, origin.y)
     await scene.talk([
-      { chara, text: 'やってきたぞ。' },
-      { chara, text: 'いい感じだ。' },
+      { chara, text: 'よしよし！やってきたぞ！' },
+      { chara, text: 'いい感じだ！' },
       { chara: 'ann', text: '何をしてきたの？' },
-      { chara, text: 'ヤツに酒を飲ませた。' },
+      { chara, text: 'ヤツに酒を飲ませたのさ。' },
       { chara, text: '1バレルきっちり飲んでヘロヘロしてやがるぜ。' },
       { chara, text: '今日の分の酒は失ったが、アイツがくたばるなら長い目で見て得ってもんだ。' },
       { chara, text: 'さあ、行ってこい。' }
@@ -51,11 +57,21 @@ export const dionysus = (scene, dionysus, area, gate) => {
   dionysus.setTapEvent(async () => {
     if (state.completed) {
       await scene.talk([
-        { chara, text: 'よう' }
+        { chara, text: 'じゃあな、気をつけるんだぞ。' }
       ])
     } else if (state.solved) {
       await scene.talk([
-        { chara, text: 'せんきゅー' }
+        { chara, text: '無事か！' },
+        { chara, text: '倒したんだな？' },
+        { chara, text: 'やるじゃないか！' },
+        { chara, text: 'これでもうアイツに怯えなくて済むんだな。' },
+        { chara, text: 'よっしゃ、あんたらにこれをやるよ。' }
+      ])
+      scene.ui.increaseWeapon(7)
+      await scene.talk([
+        { chara, text: '俺が持ってるより役に立つだろう。' },
+        { chara: 'ann', text: 'ありがとう！' },
+        { chara, text: 'さ、じゃあまたな譲ちゃんたち。' }
       ])
       state.completed = true
       scene.ui.missionUpdate('m2_3', true)
@@ -78,16 +94,16 @@ export const dionysus = (scene, dionysus, area, gate) => {
         { chara, text: 'ち、違う。' },
         { chara: 'ann', text: 'じゃあその樽には何が入っているの？' },
         { chara, text: '…。' },
-        { chara, text: 'ああそうだよ。これは酒だ。' },
+        { chara, text: 'あーあーそうだよ。これは酒だ。' },
         { chara: 'ann', text: 'やっぱり。' },
         { chara, text: '俺を城の人間に付き出そうってのか？' },
-        { chara, text: '望みはなんだ？' },
+        { chara, text: '望みはなんだってんだ？' },
         { chara: 'ann', text: 'そういうわけじゃないんだけど…、' },
         { chara: 'ann', text: '詳しい話を教えてもらえない？' },
         { chara: 'ann', text: 'この地下通路のことでも、城のことでも。' },
-        { chara, text: 'なんだ、城の関係者かと思って焦ったぜ。' },
+        { chara, text: 'なんだ、城の関係者かと思って焦ったじゃねえかよ。' },
         { chara, text: '知っていると思うが、この地下通路は王城に繋がっている。' },
-        { chara, text: 'この地下通路を最奥まで進むと扉があって、その先は城の貯蔵庫だ。' },
+        { chara, text: '最奥まで進むと扉があって、その先は城の貯蔵庫だ。' },
         { chara: 'ann', text: 'お酒はお城に届けるの？' },
         { chara, text: 'ああ。密造酒は城の奴らと取引している。' },
         { chara, text: '毎日1バレルを地下通路の最奥まで運んで、' },
