40fca3abfd3b58d0606b46518f9fef4a920a91bf
laineus <lainen000@gmail.com>
2020-02-12 01:05:55 +0900

    ferdinand

--- src/map/catacomb3.js
+++ src/map/catacomb3.js
@@ -1,5 +1,7 @@
+import { ferdinand } from '../event/ferdinand'
 export default {
   create (scene) {
-    const ferdinand = scene.map.getObjectById(3)
+    const fdn = scene.map.getObjectById(3)
+    ferdinand(scene, fdn)
   }
 }
