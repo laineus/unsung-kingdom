b05d4b33b75642716e3606371b7e49abf1f31b79
laineus <lainen000@gmail.com>
2020-02-24 16:37:59 +0900

    make face origin bottom

--- src/class/SpeachBubble.js
+++ src/class/SpeachBubble.js
@@ -21,7 +21,7 @@ export default class SpeachBubble extends Phaser.GameObjects.Container {
     this.bg.strokeColor = config.COLORS.white
     const left = position === -1
     // const mask = scene.make.graphics().setPosition(this.x + 80 * position, this.y - 29).fillRect(-80, -55, 160, 120).setRotation(-0.05 * position).createGeometryMask()
-    this.image = scene.add.sprite(left ? -120 : 102, -100, imageKey).setOrigin(0.5, 0)
+    this.image = scene.add.sprite(left ? -120 : 102, 50, imageKey).setOrigin(0.5, 1)
     this.name = scene.add.text(left ? -70 : -140, left ? -69 : -72, name, { fill: config.COLORS.white.toColorString, fontSize: 16, fontFamily: config.FONTS.TEXT, fontStyle: 'bold', stroke: config.COLORS.dark.toColorString, strokeThickness: 2 }).setPadding(0, 2, 0, 0)
     this.text = scene.add.text(left ? 44 : -44, -6, '', { fill: config.COLORS.white.toColorString, fontSize: 14, fontFamily: config.FONTS.TEXT, lineSpacing: 4.5, stroke: config.COLORS.dark.toColorString, strokeThickness: 1.5 }).setOrigin(0.5, 0.5).setPadding(0, 2, 0, 0)
     this.setText(text)
