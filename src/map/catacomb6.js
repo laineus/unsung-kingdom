db18eb52f0a16b7b178cfd8f9199b88f8f2ada2d
laineus <lainen000@gmail.com>
2020-02-24 16:03:35 +0900

    catacomb6 tresure

--- src/map/catacomb6.js
+++ src/map/catacomb6.js
@@ -6,5 +6,36 @@ export default {
     ['spectre']
   ],
   create (scene) {
+    const state = scene.storage.state
+    const earned = state.treasures.includes('catacomb6_9')
+    scene.map.treasures[0].removeTapEvent()
+    const doorEv = scene.map.getObjectById(10)
+    if (earned) {
+      this.openDoor(scene)
+    } else if (state.event.m3_4.completed) {
+      doorEv.setTapEvent(async () => {
+        await scene.talk([
+          { chara: 'ann', text: 'さっきもらった鍵がぴったりだ！' }
+        ])
+        this.openDoor(scene)
+        doorEv.destroy()
+      })
+    }
+  },
+  openDoor (scene) {
+    const layer3 = scene.map.getLayerByName('layer3')
+    const doorArea = [
+      [14, 4],
+      [15, 4],
+      [16, 4],
+      [14, 5],
+      [15, 5],
+      [16, 5]
+    ]
+    doorArea.forEach(data => {
+      layer3.layer.data[data[1]][data[0]].index = -1
+      layer3.layer.data[data[1]][data[0]].setCollision(false)
+    })
+    scene.map.treasures[0].setEvent()
   }
 }
