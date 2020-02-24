5988b372a7d4f0c7ff2168e45bbe143d10672417
laineus <lainen000@gmail.com>
2020-02-25 00:04:59 +0900

    gimmick

--- src/map/underpass8.js
+++ src/map/underpass8.js
@@ -9,5 +9,39 @@ export default {
   ],
   create (scene) {
     jack(scene, scene.map.getObjectById(3), scene.map.getObjectById(4))
+    const fire = scene.map.getObjectById(5)
+    if (scene.storage.state.gimmicks.includes('unserpass8_5')) {
+      this.openNeedle(scene, false)
+      fire.destroy()
+    } else {
+      fire.setTapEvent(async () => {
+        scene.storage.state.gimmicks.push('unserpass8_5')
+        await scene.talk([
+          { chara: 'ann', text: '燭台の火が消えてる…。' }
+        ])
+        await scene.ui.sleep(500)
+        await scene.player.setTargetPosition((28).toPixelCenter, (40).toPixelCenter)
+        scene.player.setR('right')
+        await scene.ui.sleep(1000)
+        await scene.player.setTargetPosition((26).toPixelCenter, (40).toPixelCenter)
+        scene.player.setR('left')
+        await scene.ui.sleep(500)
+        await this.openNeedle(scene, true)
+        fire.destroy()
+      })
+    }
+  },
+  async openNeedle (scene, event) {
+    const layer4 = scene.map.getLayerByName('layer4')
+    layer4.layer.data[40][25].index = 59
+    if (event) await scene.ui.sleep(500)
+    if (event) await scene.camera.look(-400, -450, 1000)
+    if (event) await scene.ui.sleep(100)
+    Array(23, 24, 25, 26, 27, 28).forEach(y => {
+      layer4.layer.data[y][11].index = 79
+      layer4.layer.data[y][11].setCollision(false)
+    })
+    if (event) await scene.ui.sleep(500)
+    if (event) await scene.camera.look(400, 450, 1000)
   }
 }
