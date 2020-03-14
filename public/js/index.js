b95b9bea5142abace6cb2012bc36032eeab6e5dc
laineus <lainen000@gmail.com>
2020-03-15 01:27:46 +0900

    esc

--- public/js/index.js
+++ public/js/index.js
@@ -3,4 +3,8 @@ window.addEventListener('DOMContentLoaded', () => {
     e.preventDefault()
     document.body.classList.add('fullscreen')
   })
+  document.body.addEventListener('keydown', e => {
+    if (e.keyCode !== 27) return
+    document.body.classList.remove('fullscreen')
+  })
 })
