faeb9bbd5fa7f20191a4391244701bf422eb2f45
laineus <lainen000@gmail.com>
2020-05-10 01:04:37 +0900

    book events

--- src/map/room1.js
+++ src/map/room1.js
@@ -2,7 +2,8 @@ import { execChapterBeginEvents } from '../event/room'
 export default {
   name: '王都 - 拠点',
   create (scene) {
-    execChapterBeginEvents(scene)
+    const book = scene.map.getObjectById(13)
+    execChapterBeginEvents(scene, book)
     scene.map.getObjectById(11).setEvent(async () => {
       scene.francisca.setTarget(scene.player)
       scene.jaquelyn.setTarget(scene.player)
