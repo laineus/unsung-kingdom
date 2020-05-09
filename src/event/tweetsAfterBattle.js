358dadbd2e9c7528902283da7dd9b63f68a1588a
laineus <lainen000@gmail.com>
2020-05-10 02:18:11 +0900

    tweets

--- src/event/tweetsAfterBattle.js
+++ src/event/tweetsAfterBattle.js
@@ -3,8 +3,9 @@ import { MAGIC_STONES } from './princess'
 
 export default scene => {
   if (scene.storage.state.event.m1_3.count >= MANDRAKE_COUNT) {
-    scene.tweetOnce(scene.jaquelyn, 'マンドレイクの根が集まったみたいね', 'mandrake')
-  } else if (scene.storage.state.event.m2_2.count >= MAGIC_STONES) {
-    scene.tweetOnce(scene.francisca, 'カーバンクルの魔石が集まったね', 'mandrake')
+    scene.tweetOnce(scene.jaquelyn, 'マンドレイクの根が集まったみたいね', 'mdrk')
+  }
+  if (scene.storage.state.event.m2_2.count >= MAGIC_STONES) {
+    scene.tweetOnce(scene.francisca, 'カーバンクルの魔石が集まったね', 'mstn')
   }
 }
