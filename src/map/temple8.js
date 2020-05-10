b56532f6539d4345125b679a06cb325c6384646c
laineus <lainen000@gmail.com>
2020-05-10 17:46:21 +0900

    tweet apl

--- src/map/temple8.js
+++ src/map/temple8.js
@@ -1,4 +1,4 @@
-import { appleCollection } from '../event/lute'
+import { appleCollection, GOOD_APPLES } from '../event/lute'
 export default {
   name: 'グリファルデ神殿 - 東部',
   enemyLevel: 38,
@@ -8,6 +8,15 @@ export default {
     ['bird', 'tree', 'bird']
   ],
   create (scene) {
-    Array.range(4, 9).forEach(id => appleCollection(scene, scene.map.getObjectById(id), `a8_${id}`))
+    this.apples = Array.range(4, 9).map(id => {
+      return { apple: scene.map.getObjectById(id), key: `a8_${id}` }
+    })
+    this.apples.forEach(v => appleCollection(scene, v.apple, v.key))
+  },
+  update (scene) {
+    if (scene.frame % 15 !== 0) return
+    this.apples.filter(v => v.apple.distanceToPlayer < 80).forEach(v => {
+      GOOD_APPLES.includes(v.key) ? scene.tweetOnce(scene.player, 'おいしそうなリンゴ', 'aplg') : scene.tweetOnce(scene.francisca, '傷んだリンゴだね', 'aplb')
+    })
   }
 }
