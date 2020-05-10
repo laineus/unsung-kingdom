ac9390b22a86f4ed60fbe4bbd93d9c8265245a3c
laineus <lainen000@gmail.com>
2020-05-10 17:03:14 +0900

    save

--- src/event/tweetsAfterBattle.js
+++ src/event/tweetsAfterBattle.js
@@ -1,5 +1,7 @@
 import { MANDRAKE_COUNT } from './drystan'
 import { MAGIC_STONES } from './princess'
+import { WATER_COUNT } from './marianne'
+import { KELUNNE_COUNT } from './allsRight'
 
 export default scene => {
   if (scene.storage.state.event.m1_3.count >= MANDRAKE_COUNT) {
@@ -7,5 +9,11 @@ export default scene => {
   }
   if (scene.storage.state.event.m2_2.count >= MAGIC_STONES) {
     scene.tweetOnce(scene.francisca, 'カーバンクルの魔石が集まったね', 'mstn')
+  }
+  if (scene.storage.state.event.m3_2.count >= WATER_COUNT) {
+    scene.tweetOnce(scene.francisca, '飲み水が十分に集まったね', 'wtr')
+  }
+  if (scene.storage.state.event.m4_3.count >= KELUNNE_COUNT) {
+    scene.tweetOnce(scene.jaquelyn, 'ケルーネの羽根が集まったね', 'kln')
   }
 }
