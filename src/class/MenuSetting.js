eea8045765615cdfba8eab2552afd8acba316812
laineus <lainen000@gmail.com>
2020-05-22 00:43:15 +0900

    menu setting

--- src/class/MenuSetting.js
+++ src/class/MenuSetting.js
@@ -1,5 +1,6 @@
 import config from '../data/config'
 import Slider from './Slider'
+import { slideIn } from '../util/animations'
 export default class MenuSetting extends Phaser.GameObjects.Container {
   constructor (scene) {
     super(scene)
@@ -7,18 +8,24 @@ export default class MenuSetting extends Phaser.GameObjects.Container {
     const title = scene.add.text(20, 15, 'SETTINGS', { align: 'center', fill: config.COLORS.theme.toColorString, fontSize: 25, fontStyle: 'bold', fontFamily: config.FONTS.UI })
     const sub = scene.add.text(20, 41, '設定', { align: 'center', fill: config.COLORS.gray.toColorString, fontSize: 10, fontStyle: 'bold', fontFamily: config.FONTS.TEXT })
     this.add([title, sub])
-    const contentX = 200
-    // BGM
-    const bgmTitle = scene.add.text(contentX, 120, 'BGM Volume :', { fill: config.COLORS.gray.toColorString, fontSize: 14, fontStyle: 'bold', fontFamily: config.FONTS.TEXT })
-    const bgmSlider = new Slider(scene, contentX + 200, 150, 400, 10, { value: scene.setting.state.bgm })
-    const bgmValue = scene.add.text(contentX + 430, 141, `${scene.setting.state.bgm}%`, { fill: config.COLORS.white.toColorString, fontSize: 14, fontStyle: 'bold', fontFamily: config.FONTS.TEXT })
-    bgmSlider.on('updated', value => {
+    const bgm = this.getVolumeSetting(220, 120, 'bgm', 'BGM')
+    const se = this.getVolumeSetting(220, 200, 'se', 'SE')
+    this.add([bgm, se])
+    slideIn(scene, [bgm, se], { x: -100 })
+  }
+  getVolumeSetting (x, y, key, name) {
+    const container = this.scene.add.container(x, y)
+    const title = this.scene.add.text(0, 0, `${name} Volume :`, { fill: config.COLORS.gray.toColorString, fontSize: 14, fontStyle: 'bold', fontFamily: config.FONTS.TEXT })
+    const slider = new Slider(this.scene, 200, 33, 400, 10, { value: this.scene.setting.state[key] })
+    const valueLabel = this.scene.add.text(430, 24, `${this.scene.setting.state[key]}%`, { fill: config.COLORS.white.toColorString, fontSize: 14, fontStyle: 'bold', fontFamily: config.FONTS.TEXT })
+    slider.on('updated', value => {
       const v = Math.round(value)
-      bgmValue.setText(`${v}%`)
-      scene.setting.state.bgm = v
-      scene.setting.save()
-      scene.audio.updateBgmVolume()
+      valueLabel.setText(`${v}%`)
+      this.scene.setting.state[key] = v
+      this.scene.setting.save()
+      if (key === 'bgm') this.scene.audio.updateBgmVolume()
     })
-    this.add([bgmTitle, bgmSlider, bgmValue])
+    container.add([title, slider, valueLabel])
+    return container
   }
 }
