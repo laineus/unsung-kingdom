cb7a713f0ad13534f9a7cb244c87c302f627d947
laineus <lainen000@gmail.com>
2020-05-21 23:31:38 +0900

    CommonSetting

---
+++ src/class/CommonSetting.js
@@ -0,0 +1,25 @@
+export default class CommonSetting {
+  constructor () {
+    this.state = this.getSetting()
+  }
+  getSetting () {
+    const settingString = localStorage.getItem('setting')
+    if (!settingString) return this.getDefaultSetting()
+    try {
+      return JSON.parse(settingString)
+    } catch (error) {
+      return this.getDefaultSetting()
+    }
+  }
+  save () {
+    const json = JSON.stringify(this.state)
+    localStorage.setItem('setting', json)
+  }
+  getDefaultSetting () {
+    return {
+      bgm: 100,
+      se: 100,
+      controller: 'auto'
+    }
+  }
+}
---
+++ src/data/setting.js
@@ -0,0 +1,2 @@
+import CommonSetting from '../class/CommonSetting'
+export default new CommonSetting()
