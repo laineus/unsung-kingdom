3a09fb4e0ef2e80f67783be381de921dc9fcacd2
laineus <lainen000@gmail.com>
2020-05-21 23:07:17 +0900

    settin ui

--- src/class/MenuSetting.js
+++ src/class/MenuSetting.js
@@ -1,4 +1,5 @@
 import config from '../data/config'
+import Slider from './Slider'
 export default class MenuSetting extends Phaser.GameObjects.Container {
   constructor (scene) {
     super(scene)
@@ -6,5 +7,14 @@ export default class MenuSetting extends Phaser.GameObjects.Container {
     const title = scene.add.text(20, 15, 'SETTINGS', { align: 'center', fill: config.COLORS.theme.toColorString, fontSize: 25, fontStyle: 'bold', fontFamily: config.FONTS.UI })
     const sub = scene.add.text(20, 41, '設定', { align: 'center', fill: config.COLORS.gray.toColorString, fontSize: 10, fontStyle: 'bold', fontFamily: config.FONTS.TEXT })
     this.add([title, sub])
+    const contentX = 200
+    const bgmTitle = scene.add.text(contentX, 120, 'BGM Volume :', { fill: config.COLORS.gray.toColorString, fontSize: 14, fontStyle: 'bold', fontFamily: config.FONTS.TEXT })
+    const bgmSlider = new Slider(scene, contentX + 200, 150)
+    const bgmValue = scene.add.text(contentX + 430, 141, '100%', { fill: config.COLORS.white.toColorString, fontSize: 14, fontStyle: 'bold', fontFamily: config.FONTS.TEXT })
+    bgmSlider.on('updated', value => {
+      const v = Math.round(value)
+      bgmValue.setText(`${v}%`)
+    })
+    this.add([bgmTitle, bgmSlider, bgmValue])
   }
 }
