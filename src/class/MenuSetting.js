e9bfcd120c7d782a541e0d992a5788949576ab1d
laineus <lainen000@gmail.com>
2020-05-23 00:33:20 +0900

    control setting

--- src/class/MenuSetting.js
+++ src/class/MenuSetting.js
@@ -1,6 +1,7 @@
 import config from '../data/config'
 import Slider from './Slider'
 import { slideIn } from '../util/animations'
+import Radio from './Radio'
 export default class MenuSetting extends Phaser.GameObjects.Container {
   constructor (scene) {
     super(scene)
@@ -10,14 +11,15 @@ export default class MenuSetting extends Phaser.GameObjects.Container {
     this.add([title, sub])
     const bgm = this.getVolumeSetting(220, 120, 'bgm', 'BGM')
     const se = this.getVolumeSetting(220, 200, 'se', 'SE')
-    this.add([bgm, se])
-    slideIn(scene, [bgm, se], { x: -100 })
+    const controlMode = this.getControlMode(220, 280)
+    this.add([bgm, se, controlMode])
+    slideIn(scene, [bgm, se, controlMode], { x: -100 })
   }
   getVolumeSetting (x, y, key, name) {
     const container = this.scene.add.container(x, y)
     const title = this.scene.add.text(0, 0, `${name} Volume :`, { fill: config.COLORS.gray.toColorString, fontSize: 14, fontStyle: 'bold', fontFamily: config.FONTS.TEXT })
-    const slider = new Slider(this.scene, 200, 33, 400, 10, { value: this.scene.setting.state[key] })
-    const valueLabel = this.scene.add.text(430, 24, `${this.scene.setting.state[key]}%`, { fill: config.COLORS.white.toColorString, fontSize: 14, fontStyle: 'bold', fontFamily: config.FONTS.TEXT })
+    const slider = new Slider(this.scene, 200, 35, 400, 10, { value: this.scene.setting.state[key] })
+    const valueLabel = this.scene.add.text(430, 26, `${this.scene.setting.state[key]}%`, { fill: config.COLORS.white.toColorString, fontSize: 14, fontStyle: 'bold', fontFamily: config.FONTS.TEXT })
     slider.on('updated', value => {
       const v = Math.round(value)
       valueLabel.setText(`${v}%`)
@@ -28,6 +30,13 @@ export default class MenuSetting extends Phaser.GameObjects.Container {
       if (key === 'se') this.scene.audio.se('announce')
     })
     container.add([title, slider, valueLabel])
+    return container
+  }
+  getControlMode (x, y) {
+    const container = this.scene.add.container(x, y)
+    const title = this.scene.add.text(0, 0, 'UI Type :', { fill: config.COLORS.gray.toColorString, fontSize: 14, fontStyle: 'bold', fontFamily: config.FONTS.TEXT })
+    const radio = new Radio(this.scene, 0, 40, ['Auto Detection', 'Click to Walk', 'Virtual Stick'])
+    container.add([title, radio])
     return container
   }
 }
