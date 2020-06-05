5954b5617d3b7cfa821f328e88f6051bbdfd3be1
laineus <lainen000@gmail.com>
2020-06-06 01:22:01 +0900

    add ogg

--- build/assetSettings.js
+++ build/assetSettings.js
@@ -8,5 +8,5 @@ module.exports = [
   { key: 'tilesetTiles', prefix: '', dir: '/map', rule: /^\w+\D\.json$/ },
   { key: 'tilesetMaps', prefix: '', dir: '/map', rule: /^\w+\d+\.json$/ },
   { key: 'se', prefix: 'se/', dir: '/audio/se', rule: /^\w+\.wav$/ },
-  { key: 'bgm', prefix: 'bgm/', dir: '/audio/bgm', rule: /^\w+\.m4a$/ }
+  { key: 'bgm', prefix: 'bgm/', dir: '/audio/bgm', rule: /^\w+\.(m4a|ogg)$/ }
 ]
