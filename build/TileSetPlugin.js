778fbb840e8e7e8fb905163c01381773abcf8ffa
laineus <lainen000@gmail.com>
2020-06-13 21:52:39 +0900

    pngquant for tileset

--- build/TileSetPlugin.js
+++ build/TileSetPlugin.js
@@ -1,4 +1,6 @@
 const { extrudeTilesetToImage } = require('tile-extruder')
+const imagemin = require('imagemin')
+const imageminPngquant = require('imagemin-pngquant')
 const fs = require('fs')
 
 const DIR = './public/img/tilesets'
@@ -14,7 +16,14 @@ module.exports = class {
           extrudeTilesetToImage(32, 32, `${ORIGINAL}/${file}`, `${EXTRUDED}/${file}`)
         })
         Promise.all(promises).then(() => {
-          console.log('End: TileSet')
+          imagemin([`${EXTRUDED}/*.png`], {
+            destination: EXTRUDED,
+            plugins: [
+              imageminPngquant()
+            ]
+          }).then(() => {
+            console.log('End: TileSet')
+          })
         })
       })
     })
