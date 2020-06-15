d8ba78ed2269fbe5f2aed97ba675b0674cd00a4d
laineus <lainen000@gmail.com>
2020-06-16 01:06:16 +0900

    tweets after battle

--- src/event/tweetsAfterBattle.js
+++ src/event/tweetsAfterBattle.js
@@ -5,15 +5,15 @@ import { KELUNNE_COUNT } from './allsRight'
 
 export default scene => {
   if (scene.storage.state.event.m1_3.count >= MANDRAKE_COUNT) {
-    scene.tweetOnce(scene.jaquelyn, 'マンドレイクの根が集まったみたいね', 'mdrk')
+    scene.tweetOnce(scene.jaquelyn, t('afterBattle.m1_3'), 'mdrk')
   }
   if (scene.storage.state.event.m2_2.count >= MAGIC_STONES) {
-    scene.tweetOnce(scene.francisca, 'カーバンクルの魔石が集まったね', 'mstn')
+    scene.tweetOnce(scene.francisca, t('afterBattle.m2_2'), 'mstn')
   }
   if (scene.storage.state.event.m3_2.count >= WATER_COUNT) {
-    scene.tweetOnce(scene.francisca, '飲み水が十分に集まったね', 'wtr')
+    scene.tweetOnce(scene.francisca, t('afterBattle.m3_2'), 'wtr')
   }
   if (scene.storage.state.event.m4_2.count >= KELUNNE_COUNT) {
-    scene.tweetOnce(scene.jaquelyn, 'ケルーネの羽根が集まったね', 'kln')
+    scene.tweetOnce(scene.jaquelyn, t('afterBattle.m4_2'), 'kln')
   }
 }
