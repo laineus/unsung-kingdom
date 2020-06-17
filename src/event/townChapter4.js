2a5b7e96339237725332c0b3925c1ae088710092
laineus <lainen000@gmail.com>
2020-06-17 16:57:56 +0900

    townChapter4

--- src/event/townChapter4.js
+++ src/event/townChapter4.js
@@ -5,9 +5,9 @@ export default (scene, charas) => {
     if (scene.storage.state.allowed_area >= 5) return
     await scene.ui.sleep(500)
     await scene.talk([
-      { chara: 'ann', text: 'よし、' },
-      { chara: 'ann', text: '行き先は一つだね！' },
-      { chara: 'ann', text: '行こう！' }
+      { chara: 'ann', text: t('townChapter4.ann.0') },
+      { chara: 'ann', text: t('townChapter4.ann.1') },
+      { chara: 'ann', text: t('townChapter4.ann.2') }
     ])
     scene.storage.state.allowed_area = 5
     scene.ui.announce('マップ「グリファルデ神殿」が解放された')
@@ -19,12 +19,12 @@ export default (scene, charas) => {
   // 噂好きのアンバー婦人
   charas.amber.setTapEvent(async chara => {
     await scene.talk([
-      { chara, text: '大変よ、' },
-      { chara, text: 'グリファルデ神殿に封じられていたドラゴンが目覚めたの。' },
-      { chara, text: 'それで王と王弟が率いる討伐隊がグリファルデ神殿に向かったわ。' },
-      { chara, text: 'こんなことになるなんて…、' },
-      { chara, text: '大丈夫かしら…。' },
-      { chara, text: 'きっとあのドラゴンは、この国に十年前の復讐をするつもりなんだわ。' }
+      { chara, text: t('townChapter4.amber.0') },
+      { chara, text: t('townChapter4.amber.1') },
+      { chara, text: t('townChapter4.amber.2') },
+      { chara, text: t('townChapter4.amber.3') },
+      { chara, text: t('townChapter4.amber.4') },
+      { chara, text: t('townChapter4.amber.5') }
     ])
     eState.talked_amber = true
     await allowArea()
@@ -32,11 +32,11 @@ export default (scene, charas) => {
   // 宿屋のアナベル
   charas.annabelle.setTapEvent(async chara => {
     await scene.talk([
-      { chara, text: 'ドラゴンが目覚めたって噂、本当だったんだ。' },
-      { chara, text: 'ここ最近、王弟がよく神殿に通ってたみたいだけど、' },
-      { chara, text: 'きっとドラゴンの様子を見張っていたのね。' },
-      { chara, text: 'はぁ、' },
-      { chara, text: 'うまくいって平和になるといいんだけど。' }
+      { chara, text: t('townChapter4.annabelle.0') },
+      { chara, text: t('townChapter4.annabelle.1') },
+      { chara, text: t('townChapter4.annabelle.2') },
+      { chara, text: t('townChapter4.annabelle.3') },
+      { chara, text: t('townChapter4.annabelle.4') }
     ])
     eState.talked_annabelle = true
     await allowArea()
@@ -44,11 +44,11 @@ export default (scene, charas) => {
   // 内気なマチルダ
   charas.matilda.setTapEvent(async chara => {
     await scene.talk([
-      { chara, text: 'ドラゴンって見たことないけれど、とても大きいんだよね。' },
-      { chara, text: '人間が剣で戦って倒すだなんて、想像つかないな…。' },
-      { chara, text: 'どうなっちゃうんだろう…。' },
-      { chara, text: '十年前に起きたことはよく知らないけど、ドラゴンはきっと私たちを恨んでいるんだよね…。' },
-      { chara, text: 'こわいな…。' }
+      { chara, text: t('townChapter4.matilda.0') },
+      { chara, text: t('townChapter4.matilda.1') },
+      { chara, text: t('townChapter4.matilda.2') },
+      { chara, text: t('townChapter4.matilda.3') },
+      { chara, text: t('townChapter4.matilda.4') }
     ])
     eState.talked_matilda = true
     await allowArea()
@@ -56,12 +56,12 @@ export default (scene, charas) => {
   // 卑劣なエリオット
   charas.elliott.setTapEvent(async chara => {
     await scene.talk([
-      { chara, text: '王自らドラゴン討伐に向かうなんて、' },
-      { chara, text: 'もう若くないのによくやるよ。' },
-      { chara, text: '十年前みたいにうまくやれるとは限らねえぞ。' },
-      { chara, text: 'あの時だって、倒すどころか神殿に封じ込めるだけで奇跡的だったんだ。' },
-      { chara, text: 'もし失敗したらこの国は…、' },
-      { chara, text: '考えたくもねえ。' }
+      { chara, text: t('townChapter4.elliott.0') },
+      { chara, text: t('townChapter4.elliott.1') },
+      { chara, text: t('townChapter4.elliott.2') },
+      { chara, text: t('townChapter4.elliott.3') },
+      { chara, text: t('townChapter4.elliott.4') },
+      { chara, text: t('townChapter4.elliott.5') }
     ])
     eState.talked_elliott = true
     await allowArea()
@@ -69,13 +69,13 @@ export default (scene, charas) => {
   // 賞金稼ぎのマックス
   charas.max.setTapEvent(async chara => {
     await scene.talk([
-      { chara, text: '王と王弟が率いる討伐隊がドラゴン退治に向かったらしい。' },
-      { chara, text: '半月ほど前に流れていたドラゴンが目覚めたって噂、本当だったんだな。' },
-      { chara, text: '黙って帰りを待ってるしかないなんて、情けない話だな。' },
-      { chara, text: 'でも俺は、エドガー王ならまたやれってくれるって信じてるぜ。' },
-      { chara, text: 'もし…、' },
-      { chara, text: 'もし止められなかったら、きっとあのドラゴンはこの国を襲いに来るはずだ。' },
-      { chara, text: 'そのときは俺も久々に剣を抜くぜ。' }
+      { chara, text: t('townChapter4.max.0') },
+      { chara, text: t('townChapter4.max.1') },
+      { chara, text: t('townChapter4.max.2') },
+      { chara, text: t('townChapter4.max.3') },
+      { chara, text: t('townChapter4.max.4') },
+      { chara, text: t('townChapter4.max.5') },
+      { chara, text: t('townChapter4.max.6') }
     ])
     eState.talked_max = true
     await allowArea()
