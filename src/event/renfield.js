b579277b1e5f1a736ab99f6e738942c69439663f
laineus <lainen000@gmail.com>
2020-06-18 22:48:38 +0900

    renfield

--- src/event/renfield.js
+++ src/event/renfield.js
@@ -10,27 +10,27 @@ export const renfield = (scene, ray, spectres) => {
   ray.setTapEvent(async chara => {
     if (state.ghosts.length === 5) {
       await scene.talk([
-        { chara, text: '強いんだな。' },
-        { chara, text: '…ありがとな。' },
-        { chara, text: '大したものは無いけど、' },
-        { chara, text: 'これ、さっき拾った鍵をやるよ。' },
-        { chara, text: 'どっかに使えんだろ。' },
-        { chara: 'ann', text: 'ああ、うん、ありがとう。' }
+        { chara, text: t('renfield.solve.0') },
+        { chara, text: t('renfield.solve.1') },
+        { chara, text: t('renfield.solve.2') },
+        { chara, text: t('renfield.solve.3') },
+        { chara, text: t('renfield.solve.4') },
+        { chara: 'ann', text: t('renfield.solve.5') }
       ])
-      scene.ui.announce('『錆びた鍵』を手に入れた')
+      scene.ui.announce(t('renfield.item'))
       await scene.talk([
-        { chara: 'ann', text: 'それはいいけど…、' },
-        { chara: 'ann', text: 'この後どうするの？' },
-        { chara: 'ann', text: '元に戻さないと、家族に見つかったら怒られるよ？' },
-        { chara, text: 'そんなもんいねえよ。' },
-        { chara, text: '屋敷に火をつけて…、皆殺しにしてやった。' },
-        { chara: 'ann', text: 'え、' },
-        { chara, text: 'レンフィールド家の生き残りは、遠くに住む兄弟と、俺だけだ。' },
-        { chara, text: '責めるなよ…。' },
-        { chara, text: 'そうでもしないと、犠牲になっていた人間はもっと多かったんだ…。' },
-        { chara: 'ann', text: 'そんな…。' },
-        { chara, text: '…。' },
-        { chara, text: 'じゃあな。' }
+        { chara: 'ann', text: t('renfield.solve.6') },
+        { chara: 'ann', text: t('renfield.solve.7') },
+        { chara: 'ann', text: t('renfield.solve.8') },
+        { chara, text: t('renfield.solve.9') },
+        { chara, text: t('renfield.solve.10') },
+        { chara: 'ann', text: t('renfield.solve.11') },
+        { chara, text: t('renfield.solve.12') },
+        { chara, text: t('renfield.solve.13') },
+        { chara, text: t('renfield.solve.14') },
+        { chara: 'ann', text: t('renfield.solve.15') },
+        { chara, text: t('renfield.solve.16') },
+        { chara, text: t('renfield.solve.17') }
       ])
       state.completed = true
       scene.ui.missionUpdate('m3_4', true)
@@ -39,61 +39,61 @@ export const renfield = (scene, ray, spectres) => {
       chara.destroy()
     } else if (state.started) {
       await scene.talk([
-        { chara, text: 'おい、怖いわけじゃないからな！' },
-        { chara: 'ann', text: 'じゃあなんのさ？' },
-        { chara, text: 'っ…。' },
-        { chara, text: 'うるせえ、とにかく頼む！' }
+        { chara, text: t('renfield.started.0') },
+        { chara: 'ann', text: t('renfield.started.1') },
+        { chara, text: t('renfield.started.2') },
+        { chara, text: t('renfield.started.3') }
       ])
     } else {
       await scene.talk([
-        { chara: 'ann', text: '墓荒らし！？' },
-        { chara, text: 'わ！何だお前ら！' },
-        { chara, text: '突然話しかけるな！' },
-        { chara: 'ann', text: 'あなた墓荒らしね！？' },
-        { chara, text: 'だったらなんだ、' },
-        { chara, text: 'ほっといてくれ。' },
-        { chara: 'ann', text: '放っておけるわけないないじゃない。' },
-        { chara: 'ann', text: '貴族の墓なんて掘り起こして、金目の物を漁るつもりでしょ？' },
-        { chara, text: 'ちげーよ。' },
-        { chara, text: 'ここはうちの墓だ。' },
-        { chara, text: '俺がどうしようととやかく言われる筋合いはねえ。' },
-        { chara: 'ann', text: 'うちの墓？' },
-        { chara: 'ann', text: 'それってどういうこと？' },
-        { chara, text: 'しつけーな。' },
-        { chara, text: 'ここはレンフィールド家の墓だ。' },
-        { chara, text: '汚れた人間どもの死体が埋まってる。' },
-        { chara, text: 'そして俺はレイ・レンフィールド。' },
-        { chara, text: '汚れた血筋の男であり、レンフィールド家の裏切り者だ。' },
-        { chara: 'ann', text: 'あなたのご先祖の墓なら、なおさらなんでこんな事するのよ。' },
-        { chara, text: 'レンフィールド家は汚れた家系だ。' },
-        { chara, text: 'フェルディナンド家に奴隷や少女を売って貴族になった。' },
-        { chara, text: '自分の地の民を外に売るなんて、薄汚いクズどもめ。' },
-        { chara, text: 'クズがこんな場所に埋葬される資格はないんだ。' },
-        { chara, text: '掘り起こして、燃やして、野道にでも捨ててやる。' },
-        { chara: 'ann', text: 'そんな…。' }
+        { chara: 'ann', text: t('renfield.start.0') },
+        { chara, text: t('renfield.start.1') },
+        { chara, text: t('renfield.start.2') },
+        { chara: 'ann', text: t('renfield.start.3') },
+        { chara, text: t('renfield.start.4') },
+        { chara, text: t('renfield.start.5') },
+        { chara: 'ann', text: t('renfield.start.6') },
+        { chara: 'ann', text: t('renfield.start.7') },
+        { chara, text: t('renfield.start.8') },
+        { chara, text: t('renfield.start.9') },
+        { chara, text: t('renfield.start.10') },
+        { chara: 'ann', text: t('renfield.start.11') },
+        { chara: 'ann', text: t('renfield.start.12') },
+        { chara, text: t('renfield.start.13') },
+        { chara, text: t('renfield.start.14') },
+        { chara, text: t('renfield.start.15') },
+        { chara, text: t('renfield.start.16') },
+        { chara, text: t('renfield.start.17') },
+        { chara: 'ann', text: t('renfield.start.18') },
+        { chara, text: t('renfield.start.19') },
+        { chara, text: t('renfield.start.20') },
+        { chara, text: t('renfield.start.21') },
+        { chara, text: t('renfield.start.22') },
+        { chara, text: t('renfield.start.23') },
+        { chara: 'ann', text: t('renfield.start.24') }
       ])
       await scene.camera.look(0, -150, 300, true)
       spectres.forEach(s => s.setVisible(true))
       await scene.talk([
-        { chara: 'ann', text: 'うわ！！' },
-        { chara: 'ann', text: '出た！！' }
+        { chara: 'ann', text: t('renfield.start.0') },
+        { chara: 'ann', text: t('renfield.start.1') }
       ])
       chara.setR('left')
       await scene.talk([
-        { chara, text: '！！' },
-        { chara, text: '姿を現しやがったな…、' },
-        { chara: 'ann', text: 'どうするのよ！' },
-        { chara, text: 'ふふ…、こいつらには二度死んでもらうのさ。' },
-        { chara, text: 'よ、ようし、見てろよあんたたち、' },
-        { chara, text: '…。' }
+        { chara, text: t('renfield.start.2') },
+        { chara, text: t('renfield.start.3') },
+        { chara: 'ann', text: t('renfield.start.4') },
+        { chara, text: t('renfield.start.5') },
+        { chara, text: t('renfield.start.6') },
+        { chara, text: t('renfield.start.7') }
       ], { angle: false })
       await scene.talk([
-        { chara, text: '…なあ、やっぱりあんたらに任せるよ。' },
-        { chara: 'ann', text: 'ええ！？' },
-        { chara: 'ann', text: 'やっぱり怖いんじゃない！' },
-        { chara, text: 'ち、ちげーよ！' },
-        { chara, text: 'とにかく頼む！' },
-        { chara, text: 'このままじゃやばい。' }
+        { chara, text: t('renfield.start.8') },
+        { chara: 'ann', text: t('renfield.start.9') },
+        { chara: 'ann', text: t('renfield.start.10') },
+        { chara, text: t('renfield.start.11') },
+        { chara, text: t('renfield.start.12') },
+        { chara, text: t('renfield.start.13') }
       ])
       state.started = true
       scene.ui.missionUpdate('m3_4')
