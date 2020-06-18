0f4baa469b3260cc75be83536ffc6ffd52cd492e
laineus <lainen000@gmail.com>
2020-06-19 00:48:36 +0900

    nikke

--- src/event/calibur.js
+++ src/event/calibur.js
@@ -28,10 +28,10 @@ export const calibur = (scene, sword, nikke) => {
     await fixPosition()
     const chara = nikke
     const battle = async () => {
-      const i = await scene.select(['はい', 'いいえ'])
+      const i = await scene.select([t('calibur.option.0'), t('calibur.option.1')])
       if (i === 1) {
         await scene.talk([
-          { chara, text: 'じゃあ準備ができたらまた来てね。' }
+          { chara, text: t('calibur.cancel.0') }
         ])
         eventTarget.removeTapEvent()
         nikke.setTapEvent(event)
@@ -41,35 +41,35 @@ export const calibur = (scene, sword, nikke) => {
       const result = await scene.ui.battle([generateBattler('nikke', 41, { hp: 1200 })], { boss: true, bgm: 'battle2' })
       if (!result) return
       await scene.talk([
-        { chara, text: 'うわ、' },
-        { chara, text: '凄く強いんだね。' },
-        { chara, text: 'まいったよ。降参だ。' },
-        { chara, text: 'キミたち、僕が小さいからって油断しないんだもん。' },
-        { chara, text: 'さて、剣ね。' },
-        { chara, text: '持っていって。' },
-        { chara: 'ann', text: 'ありがとう。' }
+        { chara, text: t('calibur.solve.0') },
+        { chara, text: t('calibur.solve.1') },
+        { chara, text: t('calibur.solve.2') },
+        { chara, text: t('calibur.solve.3') },
+        { chara, text: t('calibur.solve.4') },
+        { chara, text: t('calibur.solve.5') },
+        { chara: 'ann', text: t('calibur.solve.6') }
       ])
       removeSword(scene)
       scene.ui.increaseWeapon(19)
       await scene.talk([
-        { chara, text: 'うん、よく似合ってるよ。' },
-        { chara: 'ann', text: 'そうかな。' },
-        { chara, text: 'うん。' },
-        { chara, text: 'さて、' },
-        { chara, text: '剣を見張る必要もなくなったし、' },
-        { chara, text: '僕もどこかに旅に出ようかな。' }
+        { chara, text: t('calibur.solve.7') },
+        { chara: 'ann', text: t('calibur.solve.8') },
+        { chara, text: t('calibur.solve.9') },
+        { chara, text: t('calibur.solve.10') },
+        { chara, text: t('calibur.solve.11') },
+        { chara, text: t('calibur.solve.12') }
       ])
       scene.substances.remove(chara)
       await chara.setSpeed(80).setTargetPosition(chara.x - 70, chara.y + 40)
       await scene.talk([
-        { chara, text: 'あ、' },
-        { chara, text: 'ところでキミたちさ、' },
-        { chara, text: '何かズルしてない？' },
-        { chara: 'ann', text: 'え？' },
-        { chara, text: 'なんかキミたちが近くに居ると、時間が歪むのを感じるよ…。' },
-        { chara: 'ann', text: '…、' },
-        { chara, text: 'まあいいか、' },
-        { chara, text: 'それもキミたち人間の作った力だろうしね。' }
+        { chara, text: t('calibur.solve.13') },
+        { chara, text: t('calibur.solve.14') },
+        { chara, text: t('calibur.solve.15') },
+        { chara: 'ann', text: t('calibur.solve.16') },
+        { chara, text: t('calibur.solve.17') },
+        { chara: 'ann', text: t('calibur.solve.18') },
+        { chara, text: t('calibur.solve.19') },
+        { chara, text: t('calibur.solve.20') }
       ])
       await chara.setSpeed(200).setTargetPosition(chara.x - 20, chara.y + 150)
       await chara.setSpeed(400).setTargetPosition(chara.x - 30, chara.y + 300)
@@ -82,13 +82,13 @@ export const calibur = (scene, sword, nikke) => {
     if (state.talked) {
       await scene.camera.look((16).toPixel, (7).toPixelCenter, 500)
       await scene.talk([
-        { chara, text: '準備はできた？' }
+        { chara, text: t('calibur.talked.0') }
       ])
       await battle()
     } else {
       state.talked = true
       await scene.talk([
-        { chara: 'ann', text: 'よし、竜族の言葉の煙を出すよ。' }
+        { chara: 'ann', text: t('calibur.start.0') }
       ])
       scene.ui.sleep(300)
       const mist = scene.add.rectangle(0, 0, scene.map.width, scene.map.height, 0x44CC88)
@@ -104,62 +104,62 @@ export const calibur = (scene, sword, nikke) => {
       await scene.ui.sleep(1200)
       await scene.camera.look((16).toPixel, (7).toPixel, 500)
       await scene.talk([
-        { chara, text: 'やあ、' },
-        { chara, text: '僕を呼んだのはキミだね？' },
-        { chara: 'ann', text: 'えっと、ニッケさん？' },
-        { chara, text: 'うん。' },
-        { chara, text: 'ズィからの伝言を聞いたよ。' },
-        { chara, text: 'キミは？' },
-        { chara: 'ann', text: '私はアン。' },
-        { chara, text: 'アン、キミはこの剣が欲しいんだね？' },
-        { chara: 'ann', text: 'そう！貸してくれないかな？' },
-        { chara, text: 'この剣はね、結構凄いんだ。' },
-        { chara, text: '僕たち竜族にとっては中々怖いモノだよ。' },
-        { chara, text: '人に返すにはちょっと勇気がいるねー。' },
-        { chara: 'ann', text: 'うーん、そこをなんとか…。' },
-        { chara, text: 'どうしても？' },
-        { chara: 'ann', text: 'どうしても。' },
-        { chara, text: '何に使うの？' },
-        { chara: 'ann', text: 'えーと、' },
-        { chara, text: 'これで竜を殺すのかい？' },
-        { chara: 'ann', text: 'うーん…、' },
-        { chara, text: '教えてね。' },
-        { chara: 'ann', text: 'ソンベルクを倒す。' },
-        { chara, text: 'ソンベルクを殺すんだね。' },
-        { chara, text: '可哀想なソンベルク。' },
-        { chara, text: 'やんちゃな奴だけど僕の友達だよ？' },
-        { chara: 'ann', text: 'そうなの？' },
-        { chara, text: '十年も封印されて、' },
-        { chara, text: 'やっと目が覚めたっていうのに、' },
-        { chara, text: '殺されちゃうんだね。' },
-        { chara: 'ann', text: 'そう言われても…、' },
-        { chara, text: '可哀想なソンベルク。' },
-        { chara: 'ann', text: '…。' },
-        { chara, text: 'まあでも仕方ないね。' },
-        { chara, text: 'ここは今は人間の地だ。' },
-        { chara, text: '残った竜族は虐げられても文句は言えない。' },
-        { chara: 'ann', text: '虐げるつもりはないんだけど…、' },
-        { chara: 'ann', text: 'ソンベルクは説得したら王や王国を襲うのをやめるかな？' },
-        { chara, text: 'やめないと思う。' },
-        { chara: 'ann', text: 'そうなんだ…。' },
-        { chara, text: '聖剣だったね。' },
-        { chara, text: 'よし、' },
-        { chara, text: 'うん、返すよ。' },
-        { chara, text: '人が作ったものだしね。' },
-        { chara, text: '人に返すのがフェアだ。' },
-        { chara, text: 'そしてそれをどう使おうとキミたちの自由だ。' },
-        { chara: 'ann', text: 'いいの！？' },
-        { chara, text: 'うん。' },
-        { chara, text: 'でも返す相手が人なら誰でもいいとは思ってない。' },
-        { chara, text: '見て、' },
-        { chara, text: 'これを作った人間は間違いなく強いヤツだ。' },
-        { chara, text: 'キミはこれを持つにふさわしいかな？' },
-        { chara, text: '試してもいいよね？' },
-        { chara: 'ann', text: '試すって？' },
-        { chara, text: '僕と戦うんだよ。' },
-        { chara: 'ann', text: 'なるほど。' },
-        { chara, text: 'それでいいよね？' },
-        { chara, text: '準備はできてる？' }
+        { chara, text: t('calibur.start.1') },
+        { chara, text: t('calibur.start.2') },
+        { chara: 'ann', text: t('calibur.start.3') },
+        { chara, text: t('calibur.start.4') },
+        { chara, text: t('calibur.start.5') },
+        { chara, text: t('calibur.start.6') },
+        { chara: 'ann', text: t('calibur.start.7') },
+        { chara, text: t('calibur.start.8') },
+        { chara: 'ann', text: t('calibur.start.9') },
+        { chara, text: t('calibur.start.10') },
+        { chara, text: t('calibur.start.11') },
+        { chara, text: t('calibur.start.12') },
+        { chara: 'ann', text: t('calibur.start.13') },
+        { chara, text: t('calibur.start.14') },
+        { chara: 'ann', text: t('calibur.start.15') },
+        { chara, text: t('calibur.start.16') },
+        { chara: 'ann', text: t('calibur.start.17') },
+        { chara, text: t('calibur.start.18') },
+        { chara: 'ann', text: t('calibur.start.19') },
+        { chara, text: t('calibur.start.20') },
+        { chara: 'ann', text: t('calibur.start.21') },
+        { chara, text: t('calibur.start.22') },
+        { chara, text: t('calibur.start.23') },
+        { chara, text: t('calibur.start.24') },
+        { chara: 'ann', text: t('calibur.start.25') },
+        { chara, text: t('calibur.start.26') },
+        { chara, text: t('calibur.start.27') },
+        { chara, text: t('calibur.start.28') },
+        { chara: 'ann', text: t('calibur.start.29') },
+        { chara, text: t('calibur.start.30') },
+        { chara: 'ann', text: t('calibur.start.31') },
+        { chara, text: t('calibur.start.32') },
+        { chara, text: t('calibur.start.33') },
+        { chara, text: t('calibur.start.34') },
+        { chara: 'ann', text: t('calibur.start.35') },
+        { chara: 'ann', text: t('calibur.start.36') },
+        { chara, text: t('calibur.start.37') },
+        { chara: 'ann', text: t('calibur.start.38') },
+        { chara, text: t('calibur.start.39') },
+        { chara, text: t('calibur.start.40') },
+        { chara, text: t('calibur.start.41') },
+        { chara, text: t('calibur.start.42') },
+        { chara, text: t('calibur.start.43') },
+        { chara, text: t('calibur.start.44') },
+        { chara: 'ann', text: t('calibur.start.45') },
+        { chara, text: t('calibur.start.46') },
+        { chara, text: t('calibur.start.47') },
+        { chara, text: t('calibur.start.48') },
+        { chara, text: t('calibur.start.49') },
+        { chara, text: t('calibur.start.50') },
+        { chara, text: t('calibur.start.51') },
+        { chara: 'ann', text: t('calibur.start.52') },
+        { chara, text: t('calibur.start.53') },
+        { chara: 'ann', text: t('calibur.start.54') },
+        { chara, text: t('calibur.start.55') },
+        { chara, text: t('calibur.start.56') }
       ])
       await battle()
     }
