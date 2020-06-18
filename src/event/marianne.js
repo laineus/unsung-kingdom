1983318d8b2ae3e0f672a7b7b59263752d84d64c
laineus <lainen000@gmail.com>
2020-06-18 21:55:59 +0900

    marianne

--- src/event/marianne.js
+++ src/event/marianne.js
@@ -9,103 +9,103 @@ export const marianne = (scene, sister) => {
   sister.setTapEvent(async chara => {
     if (state.completed) {
       await scene.talk([
-        { chara, text: 'マリアンヌ…。' }
+        { chara, text: t('marianne.solved.0') }
       ])
     } else if (state.count >= WATER_COUNT) {
       await scene.talk([
-        { chara: 'ann', text: 'はいシスター、' },
-        { chara: 'ann', text: '余計なお世話かもしれないけど…、お水を持ってきたよ。' },
-        { chara, text: '…。' },
-        { chara, text: 'ありがとう…。' },
-        { chara, text: 'いただきますね。' },
-        { chara: 'ann', text: 'うん。' }
+        { chara: 'ann', text: t('marianne.solve.0') },
+        { chara: 'ann', text: t('marianne.solve.1') },
+        { chara, text: t('marianne.solve.2') },
+        { chara, text: t('marianne.solve.3') },
+        { chara, text: t('marianne.solve.4') },
+        { chara: 'ann', text: t('marianne.solve.5') }
       ])
       await scene.ui.sleep(500)
       await scene.talk([
-        { chara, text: 'おいしい…。' },
-        { chara: 'ann', text: 'よかった。' },
-        { chara, text: 'あの、' },
-        { chara, text: '親切にしていただいたお礼に、このお守りを受け取ってください。' }
+        { chara, text: t('marianne.solve.6') },
+        { chara: 'ann', text: t('marianne.solve.7') },
+        { chara, text: t('marianne.solve.8') },
+        { chara, text: t('marianne.solve.9') }
       ])
-      scene.ui.announce('『十字のお守り』を手に入れた')
+      scene.ui.announce(t('marianne.item'))
       await scene.talk([
-        { chara: 'ann', text: 'お礼なんていいのに。' },
-        { chara: 'ann', text: 'あ、そういえばマリアンヌは王妃と仲がよかったって言っていたよね？' },
-        { chara: 'ann', text: '王妃のこと、少しだけ聞いてもいいかな？' },
-        { chara, text: 'ええ、マリアンヌがたまに話してくれたので、知っていることでしたら。' },
-        { chara: 'ann', text: 'えっと、' },
-        { chara: 'ann', text: '王妃は王を恨んでいたの？' },
-        { chara, text: 'え、そんなことはありえません。' },
-        { chara, text: '王妃は病気で亡くなるその瞬間まで、陛下を愛していたはずです。' },
-        { chara: 'ann', text: 'そ、そうだよね。' },
-        { chara: 'ann', text: '変なこと聞いてごめん。' }
+        { chara: 'ann', text: t('marianne.solve.10') },
+        { chara: 'ann', text: t('marianne.solve.11') },
+        { chara: 'ann', text: t('marianne.solve.12') },
+        { chara, text: t('marianne.solve.13') },
+        { chara: 'ann', text: t('marianne.solve.14') },
+        { chara: 'ann', text: t('marianne.solve.15') },
+        { chara, text: t('marianne.solve.16') },
+        { chara, text: t('marianne.solve.17') },
+        { chara: 'ann', text: t('marianne.solve.18') },
+        { chara: 'ann', text: t('marianne.solve.19') }
       ])
       await scene.ui.sleep(500)
       await scene.talk([
-        { chara: 'ann', text: 'さあ、立ち上がれる？' },
-        { chara: 'ann', text: '辛かったら街まで送るよ。' },
-        { chara, text: 'いえ…、' },
-        { chara, text: '私の気持ちは変わりません。' },
-        { chara: 'ann', text: 'そんな…、' },
-        { chara, text: '私はマリアンヌのそばで、' },
-        { chara, text: 'マリアンヌの歌を聞けるのなら、' },
-        { chara, text: '地獄へだって行きたいのです。' }
+        { chara: 'ann', text: t('marianne.solve.20') },
+        { chara: 'ann', text: t('marianne.solve.21') },
+        { chara, text: t('marianne.solve.22') },
+        { chara, text: t('marianne.solve.23') },
+        { chara: 'ann', text: t('marianne.solve.24') },
+        { chara, text: t('marianne.solve.25') },
+        { chara, text: t('marianne.solve.26') },
+        { chara, text: t('marianne.solve.27') }
       ])
       state.completed = true
       scene.ui.missionUpdate('m3_2', true)
     } else if (state.started) {
       await scene.talk([
-        { chara: 'ann', text: 'ちょっと待っていてね。' },
-        { chara: 'ann', text: '飲み物か何かを探してくるよ。' },
-        { chara, text: 'え、いえ、いいんです。' },
-        { chara: 'ann', text: 'いいから。' }
+        { chara: 'ann', text: t('marianne.started.0') },
+        { chara: 'ann', text: t('marianne.started.1') },
+        { chara, text: t('marianne.started.2') },
+        { chara: 'ann', text: t('marianne.started.3') }
       ])
     } else {
       await scene.talk([
-        { chara: 'ann', text: 'あの、シスターさん、大丈夫？' },
-        { chara: 'ann', text: 'かなり具合が悪そうだけど。' },
-        { chara, text: '…。' },
-        { chara: 'ann', text: 'あのー、' },
-        { chara, text: 'あ…、はい…。' },
-        { chara: 'ann', text: 'どうしたんですか？' },
-        { chara, text: 'なにも…、大丈夫です。' },
-        { chara: 'ann', text: '大丈夫なことないよ！' },
-        { chara: 'ann', text: '今にも死んじゃいそうだよ！' },
-        { chara: 'ann', text: 'ちゃんとご飯食べてる？' },
-        { chara, text: 'いえ…、いいんです。' },
-        { chara, text: '私は、好きで死にたいのです。' },
-        { chara: 'ann', text: 'そんな。' },
-        { chara: 'ann', text: 'どうして？' },
-        { chara, text: 'マリアンヌのところに行くのです。' },
-        { chara: 'ann', text: 'マリアンヌ？' },
-        { chara, text: 'ええ、' },
-        { chara, text: 'このお墓に眠る、私の愛するマリアンヌ…。' },
-        { chara: 'ann', text: 'その人を追いかけて死にたいの？' },
-        { chara, text: 'ええ、' },
-        { chara, text: '彼女の歌声が聞けない世界など…、' },
-        { chara, text: '未練はありません。' },
-        { chara: 'ann', text: 'うーん…、そんなに好きだったんだね。' },
-        { chara: 'ann', text: 'どんな人だったの？' },
-        { chara, text: '素敵な歌声の持ち主。' },
-        { chara, text: 'マリアンヌは、聖堂でも、お城でも歌うことが許された特別な人。' },
-        { chara, text: 'ご交友の少ない王妃殿下さえも、マリアンヌにはお心開いていたほどです。' },
-        { chara, text: '彼女の歌はそれほどまでに特別でした。' },
-        { chara: 'ann', text: 'すごい人なんだね。' },
-        { chara, text: 'はい。' },
-        { chara, text: 'そして私は、そんな彼女に恋をしました。' },
-        { chara, text: '修道女の私は神様と結婚した身。' },
-        { chara, text: '許されることではありません。' },
-        { chara, text: '彼女が亡くなってから、私は心の在り処を失い、' },
-        { chara, text: 'ここへ来ました。' },
-        { chara: 'ann', text: 'そうだったんだ。' },
-        { chara: 'ann', text: 'いつからここにいるの？' },
-        { chara, text: '数日前だと思いますが、' },
-        { chara, text: '時間の感覚がぼんやりしてきて、正確には…分かりません。' },
-        { chara: 'ann', text: 'そっか、' },
-        { chara: 'ann', text: 'ちょっと待っていてね。' },
-        { chara: 'ann', text: '飲み物か何かを探してくるよ。' },
-        { chara, text: 'え、いえ、いいんです。' },
-        { chara: 'ann', text: 'いいから。' }
+        { chara: 'ann', text: t('marianne.start.0') },
+        { chara: 'ann', text: t('marianne.start.1') },
+        { chara, text: t('marianne.start.2') },
+        { chara: 'ann', text: t('marianne.start.3') },
+        { chara, text: t('marianne.start.4') },
+        { chara: 'ann', text: t('marianne.start.5') },
+        { chara, text: t('marianne.start.6') },
+        { chara: 'ann', text: t('marianne.start.7') },
+        { chara: 'ann', text: t('marianne.start.8') },
+        { chara: 'ann', text: t('marianne.start.9') },
+        { chara, text: t('marianne.start.10') },
+        { chara, text: t('marianne.start.11') },
+        { chara: 'ann', text: t('marianne.start.12') },
+        { chara: 'ann', text: t('marianne.start.13') },
+        { chara, text: t('marianne.start.14') },
+        { chara: 'ann', text: t('marianne.start.15') },
+        { chara, text: t('marianne.start.16') },
+        { chara, text: t('marianne.start.17') },
+        { chara: 'ann', text: t('marianne.start.18') },
+        { chara, text: t('marianne.start.19') },
+        { chara, text: t('marianne.start.20') },
+        { chara, text: t('marianne.start.21') },
+        { chara: 'ann', text: t('marianne.start.22') },
+        { chara: 'ann', text: t('marianne.start.23') },
+        { chara, text: t('marianne.start.24') },
+        { chara, text: t('marianne.start.25') },
+        { chara, text: t('marianne.start.26') },
+        { chara, text: t('marianne.start.27') },
+        { chara: 'ann', text: t('marianne.start.28') },
+        { chara, text: t('marianne.start.29') },
+        { chara, text: t('marianne.start.30') },
+        { chara, text: t('marianne.start.31') },
+        { chara, text: t('marianne.start.32') },
+        { chara, text: t('marianne.start.33') },
+        { chara, text: t('marianne.start.34') },
+        { chara: 'ann', text: t('marianne.start.35') },
+        { chara: 'ann', text: t('marianne.start.36') },
+        { chara, text: t('marianne.start.37') },
+        { chara, text: t('marianne.start.38') },
+        { chara: 'ann', text: t('marianne.start.39') },
+        { chara: 'ann', text: t('marianne.start.40') },
+        { chara: 'ann', text: t('marianne.start.41') },
+        { chara, text: t('marianne.start.42') },
+        { chara: 'ann', text: t('marianne.start.43') }
       ])
       state.started = true
       scene.ui.missionUpdate('m3_2')
