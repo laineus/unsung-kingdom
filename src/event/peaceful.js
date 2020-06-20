5e7fb208712473041a44c2277b7e67571b272abb
laineus <lainen000@gmail.com>
2020-06-21 01:43:56 +0900

    peaceful

--- src/event/peaceful.js
+++ src/event/peaceful.js
@@ -14,9 +14,9 @@ export const peaceful = (scene, area1, area2, soldier1, soldier2, jack) => {
     await scene.camera.look((16).toPixel, (15).toPixel, 800)
     await scene.ui.sleep(500)
     await scene.talk([
-      { chara: soldier1, text: '暗殺を企てている者の格好は黒いローブ。' },
-      { chara: soldier1, text: '王弟殿下によると、見つけ次第、即処刑してよいとのことだ。' },
-      { chara: soldier2, text: '黒いローブだな。分かった。' }
+      { chara: soldier1, text: t('peaceful.soldier.0') },
+      { chara: soldier1, text: t('peaceful.soldier.1') },
+      { chara: soldier2, text: t('peaceful.soldier.2') }
     ], { angle: false })
     await scene.ui.sleep(500)
     await scene.ui.transition('normal')
@@ -37,107 +37,107 @@ export const peaceful = (scene, area1, area2, soldier1, soldier2, jack) => {
     jack.setFaceKey('jack2')
     const chara = jack
     await scene.talk([
-      { chara, text: '来たか。' },
-      { chara: 'ann', text: 'どうしてあなたがここに？' },
-      { chara, text: 'お前たちをここへ呼び出したのは俺だ。' },
-      { chara: 'ann', text: 'え…？' },
-      { chara: 'ann', text: 'あれ、その顔の怪我は…。' },
-      { chara, text: '…。' },
-      { chara, text: '今一度、名乗らせてくれ。' }
+      { chara, text: t('peaceful.talk1.0') },
+      { chara: 'ann', text: t('peaceful.talk1.1') },
+      { chara, text: t('peaceful.talk1.2') },
+      { chara: 'ann', text: t('peaceful.talk1.3') },
+      { chara: 'ann', text: t('peaceful.talk1.4') },
+      { chara, text: t('peaceful.talk1.5') },
+      { chara, text: t('peaceful.talk1.6') }
     ])
     await scene.ui.sleep(500)
     await scene.ui.transition('fast')
     jack.initImage('kingbrother').setFaceKey('kingbrother3').setDisplayName(t('chara.ethel'))
     await scene.ui.sleep(500)
     const t = await scene.talk([
-      { chara, text: '俺は王弟エゼルバルドだ。' },
-      { chara: 'ann', text: '！' },
-      { chara, text: 'すまなかった。' },
-      { chara, text: '姿を偽ってきたこと、お前たちに剣を向けたこと。' },
-      { chara: 'ann', text: 'どういうこと？' },
-      { chara: 'ann', text: 'だって、何度も王を殺そうとして…。' },
-      { chara, text: 'ああ、間違いない。' },
-      { chara, text: '俺は何度も王を殺そうとしていた。' },
-      { chara, text: '今こそ、全てを説明させてくれ。' },
-      { chara, text: '十年前、王がドラゴンと戦ったことは知っているか？' },
+      { chara, text: t('peaceful.talk2.0') },
+      { chara: 'ann', text: t('peaceful.talk2.1') },
+      { chara, text: t('peaceful.talk2.2') },
+      { chara, text: t('peaceful.talk2.3') },
+      { chara: 'ann', text: t('peaceful.talk2.4') },
+      { chara: 'ann', text: t('peaceful.talk2.5') },
+      { chara, text: t('peaceful.talk2.6') },
+      { chara, text: t('peaceful.talk2.7') },
+      { chara, text: t('peaceful.talk2.8') },
+      { chara, text: t('peaceful.talk2.9') },
       null
     ])
-    const i = await scene.select(['知ってる', '知らない'])
+    const i = await scene.select([t('peaceful.question.0'), t('peaceful.question.1')])
     t.destroy()
     await scene.talk([
-      ...(i === 0 ? [{ chara, text: 'そう、' }] : []),
-      { chara, text: '十年前、王はソンベルクと戦い、グリファルデ神殿に封じた。' },
-      { chara, text: 'しかし半月ほど前、' },
-      { chara, text: 'グリファルデ神殿の見張りがソンベルクの覚醒を知らせたのだ。' },
-      { chara, text: '目覚めたソンベルクは王を恨み、復讐を企てていた。' },
-      { chara, text: '恨みの先である兄に代わってヤツと交渉すべく、' },
-      { chara, text: '俺は何度もグリファルデ神殿へ通っていた。' },
-      { chara, text: '様々な条件と引き換えに、王国への復讐を辞めるように説得してきたのだ。' },
-      { chara, text: 'しかし、ソンベルクの望みは一貫していた。' },
-      { chara, text: 'ついに俺は諦め、俺は王にも知らせず、ソンベルクとある契約を結んだ。' },
-      { chara, text: '王の首と引き換えに、王国の襲撃を辞めること。' },
-      { chara, text: '綺麗に言えば、俺は兄の死を代償に王国を救おうと考えたのだ。' },
-      { chara: 'ann', text: 'そうだったの…。' },
-      { chara, text: '初めてここで会った時のことを覚えているか？' },
-      { chara, text: 'あの日俺は、兄を殺そうとしていた。' },
-      { chara, text: 'だがお前たちが止めに入った。' },
-      { chara, text: 'そして俺は、お前たちからこの水晶を奪いあげたな。' },
-      { chara: 'ann', text: '時間水晶！' },
-      { chara, text: 'やはりこれは特別なものだな？' },
-      { chara, text: '直後にこの水晶は、俺を別の世界に運んだ。' },
-      { chara, text: '何が起きたか分からなかったが、ほどなくしてそこが、' },
-      { chara, text: 'ここと同じ場所の、異なる時代だと気づいた。' },
-      { chara, text: 'あれは…、' },
-      { chara, text: 'お前たちが来た時代だな？' },
-      { chara: 'ann', text: 'そう。' },
-      { chara: 'ann', text: 'その水晶は2つの時間をつなぐもの。' },
-      { chara: 'ann', text: 'あなたが行ったのは私たちの時代。' },
-      { chara: 'ann', text: 'そこで何を見たの？' },
-      { chara, text: '廃墟と化したこの王城、' },
-      { chara, text: 'その時代、この国は滅びていたのだ。' },
-      { chara: 'ann', text: 'え！？' },
-      { chara: 'ann', text: 'そんなはずは…、' },
-      { chara, text: '俺はこの目で見た。' },
-      { chara, text: 'あれは紛れもなく竜の仕業。' },
-      { chara, text: 'あの日、俺が兄を殺し損ねたことで、ソンベルクはこの国を焼き尽くした。' },
-      { chara: 'ann', text: 'そんな…。' },
-      { chara, text: 'お前たちの居た頃のこの国がどれだけ不幸だったかは知らない。' },
-      { chara, text: 'だが、ソンベルクの襲撃はなんとしても防がなければいけない。' },
-      { chara, text: '俺はそう理解した。' },
-      { chara, text: 'だから、この時代に戻ってきてからも、俺は王を殺すための別の機会を狙い続けた。' },
-      { chara: 'ann', text: '…。' },
-      { chara: 'ann', text: 'でも最後は王の暗殺はやめて、ソンベルクに立ち向かったよね？' },
-      { chara, text: '考えが変わったのだ。' },
-      { chara, text: 'いや、そもそもドラゴンを倒そうなどという考えは最初からなかった。' },
-      { chara, text: '墓地でお前たちが俺に挑み、打ち負かしたとき、' },
-      { chara, text: '俺は兄とともにドラゴンに立ち向かうことを決意した。' },
-      { chara, text: '結局は破れ、お前たちに助けられたがな。' },
-      { chara: 'ann', text: 'ソンベルクを倒したのは王だよ。' },
-      { chara, text: 'そうだったな。' },
-      { chara, text: 'そういうことにしておこう。' },
-      { chara, text: 'とにかく、これが俺のやってきたことの全てだ。' },
-      { chara, text: '呼び出してすまなかった。' },
-      { chara, text: 'この水晶を返さなければ困るだろうと思ってな。' }
+      ...(i === 0 ? [{ chara, text: t('peaceful.talk3.0') }] : []),
+      { chara, text: t('peaceful.talk3.1') },
+      { chara, text: t('peaceful.talk3.2') },
+      { chara, text: t('peaceful.talk3.3') },
+      { chara, text: t('peaceful.talk3.4') },
+      { chara, text: t('peaceful.talk3.5') },
+      { chara, text: t('peaceful.talk3.6') },
+      { chara, text: t('peaceful.talk3.7') },
+      { chara, text: t('peaceful.talk3.8') },
+      { chara, text: t('peaceful.talk3.9') },
+      { chara, text: t('peaceful.talk3.10') },
+      { chara, text: t('peaceful.talk3.11') },
+      { chara: 'ann', text: t('peaceful.talk3.12') },
+      { chara, text: t('peaceful.talk3.13') },
+      { chara, text: t('peaceful.talk3.14') },
+      { chara, text: t('peaceful.talk3.15') },
+      { chara, text: t('peaceful.talk3.16') },
+      { chara: 'ann', text: t('peaceful.talk3.17') },
+      { chara, text: t('peaceful.talk3.18') },
+      { chara, text: t('peaceful.talk3.19') },
+      { chara, text: t('peaceful.talk3.20') },
+      { chara, text: t('peaceful.talk3.21') },
+      { chara, text: t('peaceful.talk3.22') },
+      { chara, text: t('peaceful.talk3.23') },
+      { chara: 'ann', text: t('peaceful.talk3.24') },
+      { chara: 'ann', text: t('peaceful.talk3.25') },
+      { chara: 'ann', text: t('peaceful.talk3.26') },
+      { chara: 'ann', text: t('peaceful.talk3.27') },
+      { chara, text: t('peaceful.talk3.28') },
+      { chara, text: t('peaceful.talk3.29') },
+      { chara: 'ann', text: t('peaceful.talk3.30') },
+      { chara: 'ann', text: t('peaceful.talk3.31') },
+      { chara, text: t('peaceful.talk3.32') },
+      { chara, text: t('peaceful.talk3.33') },
+      { chara, text: t('peaceful.talk3.34') },
+      { chara: 'ann', text: t('peaceful.talk3.35') },
+      { chara, text: t('peaceful.talk3.36') },
+      { chara, text: t('peaceful.talk3.37') },
+      { chara, text: t('peaceful.talk3.38') },
+      { chara, text: t('peaceful.talk3.39') },
+      { chara: 'ann', text: t('peaceful.talk3.40') },
+      { chara: 'ann', text: t('peaceful.talk3.41') },
+      { chara, text: t('peaceful.talk3.42') },
+      { chara, text: t('peaceful.talk3.43') },
+      { chara, text: t('peaceful.talk3.44') },
+      { chara, text: t('peaceful.talk3.45') },
+      { chara, text: t('peaceful.talk3.46') },
+      { chara: 'ann', text: t('peaceful.talk3.47') },
+      { chara, text: t('peaceful.talk3.48') },
+      { chara, text: t('peaceful.talk3.49') },
+      { chara, text: t('peaceful.talk3.50') },
+      { chara, text: t('peaceful.talk3.51') },
+      { chara, text: t('peaceful.talk3.52') }
     ])
-    await scene.ui.announce('『時間水晶』を手に入れた')
+    await scene.ui.announce(t('peaceful.item'))
     await scene.talk([
-      { chara: 'ann', text: 'ありがとう。' },
-      { chara: 'ann', text: 'すごくびっくりしたし、' },
-      { chara: 'ann', text: 'まだちょっと頭の整理ができていないけど…、' },
-      { chara: 'ann', text: 'とにかくあなたのやってきたことを知ることができてよかったよ。' },
-      { chara: 'ann', text: 'あなたのことはずっと謎だったしね。' },
-      { chara: 'ann', text: 'あとね、' },
-      { chara: 'ann', text: '最後に暗殺を考え直してくれてよかった。' },
-      { chara: 'ann', text: '私たちはここでの目的を終えたから帰るつもりだったけど、' },
-      { chara: 'ann', text: '正直このまま帰って大丈夫か少し不安だったんだ…。' },
-      { chara: 'ann', text: 'でも、' },
-      { chara: 'ann', text: 'この時代のことは、安心して任せられるね。' },
-      { chara, text: '…。' },
-      { chara, text: 'その通り、もう心配事はないはずだ。' },
-      { chara, text: 'さあ…、帰りなさい。' },
-      { chara: 'ann', text: 'うん。' },
-      { chara: 'ann', text: '…。' },
-      { chara: 'ann', text: 'さようなら。' }
+      { chara: 'ann', text: t('peaceful.talk4.0') },
+      { chara: 'ann', text: t('peaceful.talk4.1') },
+      { chara: 'ann', text: t('peaceful.talk4.2') },
+      { chara: 'ann', text: t('peaceful.talk4.3') },
+      { chara: 'ann', text: t('peaceful.talk4.4') },
+      { chara: 'ann', text: t('peaceful.talk4.5') },
+      { chara: 'ann', text: t('peaceful.talk4.6') },
+      { chara: 'ann', text: t('peaceful.talk4.7') },
+      { chara: 'ann', text: t('peaceful.talk4.8') },
+      { chara: 'ann', text: t('peaceful.talk4.9') },
+      { chara: 'ann', text: t('peaceful.talk4.10') },
+      { chara, text: t('peaceful.talk4.11') },
+      { chara, text: t('peaceful.talk4.12') },
+      { chara, text: t('peaceful.talk4.13') },
+      { chara: 'ann', text: t('peaceful.talk4.14') },
+      { chara: 'ann', text: t('peaceful.talk4.15') },
+      { chara: 'ann', text: t('peaceful.talk4.16') }
     ])
     await scene.ui.sleep(500)
     window.archiveManager.activate('saved_kingdom')
@@ -150,17 +150,17 @@ export const peaceful = (scene, area1, area2, soldier1, soldier2, jack) => {
     await soldier2.setPosition((32).toPixelCenter, (20).toPixelCenter).setVisible(true)
     await scene.ui.sleep(1500)
     await scene.talk([
-      { chara, text: '君たちの時代が明るいことは、' },
-      { chara, text: '我らが王、' },
-      { chara, text: '平和王エドガーが保証する。' }
+      { chara, text: t('peaceful.end1.0') },
+      { chara, text: t('peaceful.end1.1') },
+      { chara, text: t('peaceful.end1.2') }
     ], { angle: false })
     await scene.ui.sleep(1500)
     soldier1.setSpeed(180).setTargetPosition(soldier1.x + 200, soldier1.y)
     await soldier2.setSpeed(180).setTargetPosition(soldier2.x + 200, soldier2.y)
     await scene.talk([
-      { chara: soldier1, text: 'おい、居たぞ！' },
-      { chara: soldier1, text: '黒いローブの暗殺者だ！' },
-      { chara: soldier2, text: '逃がすな、射て！' }
+      { chara: soldier1, text: t('peaceful.end2.0') },
+      { chara: soldier1, text: t('peaceful.end2.1') },
+      { chara: soldier2, text: t('peaceful.end2.2') }
     ], { angle: false })
     await scene.ui.sleep(800)
     const red = scene.add.rectangle(0, 0, scene.map.width, scene.map.height, 0xEE2200).setAlpha(0).setDepth(140000).setOrigin(0, 0)
@@ -170,8 +170,8 @@ export const peaceful = (scene, area1, area2, soldier1, soldier2, jack) => {
     })
     await scene.ui.sleep(2500)
     await scene.talk([
-      { chara, text: '王よ、' },
-      { chara, text: '後世でうたわれる王国を…' }
+      { chara, text: t('peaceful.end3.0') },
+      { chara, text: t('peaceful.end3.1') }
     ], { angle: false })
     await scene.ui.sleep(2000)
     await scene.ui.credit()
