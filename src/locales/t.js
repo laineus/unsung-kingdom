94af438be091e810b2e150458390a8045f98cae1
laineus <lainen000@gmail.com>
2020-07-02 00:39:08 +0900

    fix t method

--- src/locales/t.js
+++ src/locales/t.js
@@ -1,4 +1,18 @@
-import MultiLocaleString from './MultiLocaleString'
+import locales from './index'
+import setting from '../data/setting'
 export default (key, values) => {
-  return new MultiLocaleString(key, values)
+  const locale = locales[setting.state.lang]
+  const text = key.split('.').reduce((obj, k) => obj && obj[k], locale)
+  if (text === undefined) {
+    console.error(`Translation missing: "${key}" of "${setting.state.lang}"`)
+    return 'Missing'
+  }
+  if (!values) return text
+  if (values instanceof String || typeof values === 'number') {
+    return text.replace(new RegExp('\\#\\{\\w+\\}', 'g'), values)
+  } else {
+    return Object.keys(values).reduce((text, key) => {
+      return text.replace(new RegExp(`\\#\\{${key}\\}`, 'g'), values[key])
+    }, text)
+  }
 }
