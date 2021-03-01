e26f276728097b6b8c673aee36c893557af4d65a
laineus <lainen000@gmail.com>
2021-03-01 20:41:14 +0900

    fix english in japanese version

--- src/locales/ja/index.js
+++ src/locales/ja/index.js
@@ -30,14 +30,10 @@ export default {
   chapter: [
     { name: "序章", title: "王殺しのジャック", sub: "Jack the Kingkiller" },
     { name: "1章", title: "ワルカノの森", sub: "Warkano Forest" },
-    {
-      name: "2章",
-      title: "トロイア公爵邸の地下通路",
-      sub: "Troy's secret passage",
-    },
-    { name: "3章", title: "聖アンテルスの墓地", sub: "St Anterus' catacomb" },
-    { name: "4章", title: "グリファルデ神殿", sub: "Temple Grefalde" },
-    { name: "終章", title: "平和王エドガー", sub: "Edgar the Peaceful" },
+    { name: "2章", title: "トロイア公爵邸の地下通路", sub: "Troia's Secret Passage" },
+    { name: "3章", title: "聖アンテルスの墓地", sub: "Catacombs of St Anterus" },
+    { name: "4章", title: "グリファルデ神殿", sub: "Temple of Grefalde" },
+    { name: "終章", title: "平和王エドガー", sub: "Edgar the Peaceful" }
   ],
   area: {
     town: "王都",
