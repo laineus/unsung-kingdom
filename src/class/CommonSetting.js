d4cc54576144e2752d56f5a86cd7d8a638d7460b
laineus <lainen000@gmail.com>
2021-03-08 20:53:31 +0900

    default lang setting

--- src/class/CommonSetting.js
+++ src/class/CommonSetting.js
@@ -19,11 +19,17 @@ export default class CommonSetting {
     localStorage.setItem('setting', json)
   }
   getDefaultSetting () {
+    const getDefaultLang = () => {
+      const nav = window.navigator
+      if (nav.languages) return nav.languages[0]
+      return nav.language || nav.userLanguage || nav.browserLanguage
+    }
+    const lang = getDefaultLang().startsWith('ja') ? 'ja' : 'en'
     return {
       bgm: 100,
       se: 100,
       controller: 0,
-      lang: 'en'
+      lang
     }
   }
 }
